c4ad6a432e73877d5e6b2c3e4964678e571e79c2
Justin Byrne <theskaterjesus@gmail.com>
2020-10-25 22:44:38 +0000

    Added servings

---
+++ app/Models/Serving.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Serving extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'quantity',
+    ];
+
+    /**
+     * Get the meals for the serving.
+     */
+    public function meals()
+    {
+        return $this->hasMany('App\Models\Meal');
+    }
+}
