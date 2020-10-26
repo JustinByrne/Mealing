7cb7f19d7897d1b714d0b76de82ce087b3d89c93
Justin Byrne <theskaterjesus@gmail.com>
2020-10-26 01:12:43 +0000

    Created a Meal store function

--- app/Http/Controllers/MealController.php
+++ app/Http/Controllers/MealController.php
@@ -3,8 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\StoreMeal;
+use App\Models\Meal;
 
 class MealController extends Controller
 {
-    //
+    /**
+     * Create new meal
+     * 
+     * @param \App\Http\Requests\StoreMeal  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreMeal $request)
+    {
+        $meal = Meal::create($request->validated());
+    }
 }
