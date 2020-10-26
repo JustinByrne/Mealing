c70c192f90c619888c33f226f9efbcd4d12b4e79
Justin Byrne <theskaterjesus@gmail.com>
2020-10-26 01:12:58 +0000

    Added the post store route for Meal

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MealController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/meals', [MealController::class, 'store'])->name('meals.store');
