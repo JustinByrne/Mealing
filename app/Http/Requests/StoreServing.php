4a88b783aa3932510822dff078dafe81c4e7c094
Justin Byrne <theskaterjesus@gmail.com>
2020-10-27 19:23:06 +0000

    Created a request for Servings to validate

---
+++ app/Http/Requests/StoreServing.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreServing extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'quantity' => 'required',
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'quantity.required' => 'A quantity is required',
+        ];
+    }
+}
