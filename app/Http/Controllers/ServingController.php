eb41ed7980417fe610f026bbe4fefbe4aa60d430
Justin Byrne <theskaterjesus@gmail.com>
2020-10-27 19:23:27 +0000

    Created a store function for Servings

--- app/Http/Controllers/ServingController.php
+++ app/Http/Controllers/ServingController.php
@@ -3,8 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\StoreServing;
+use App\Models\Serving;
 
 class ServingController extends Controller
 {
-    //
+    /**
+     * Create new meal
+     * 
+     * @param \App\Http\Requests\StoreServing  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreServing $request)
+    {
+        $serving = Serving::create($request->validated());
+
+        return redirect($serving->path());
+    }
 }
