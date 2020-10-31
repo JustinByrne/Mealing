265b857261d8b462728cca8b274aab1a7e262a0d
Justin Byrne <theskaterjesus@gmail.com>
2020-10-31 15:20:24 +0000

    Added Requests and Models to Timing Controller

--- app/Http/Controllers/TimingController.php
+++ app/Http/Controllers/TimingController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\StoreTimingRequest;
+use App\Http\Requests\UpdateTimingRequest;
+use App\Models\Timing;
 
 class TimingController extends Controller
 {
