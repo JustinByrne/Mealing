36e8c671714da00dc9e4645b4fd61c3fd6a4b2d8
Justin Byrne <theskaterjesus@gmail.com>
2020-11-01 19:17:03 +0000

    Created Timing tests

--- tests/Feature/NewTimingTest.php
+++ tests/Feature/NewTimingTest.php
@@ -28,4 +28,56 @@ class NewTimingTest extends TestCase
         $this->assertDatabaseCount($timing->getTable(), 1);
         $response->assertRedirect($timing->path());
     }
+
+    /**
+     * test create new timing without timeFrame.
+     * 
+     * @return void
+     */
+    public function testNewTimingWithTimeFrameNull()
+    {
+        $response = $this->post(route('timing.store'), [
+            'timeFrame' => null,
+        ]);
+
+        $response->assertSessionHasErrors(['timeFrame']);
+    }
+
+    /**
+     * test update timing.
+     * 
+     * @return void
+     */
+    public function testUpdateTiming()
+    {
+        $timing = Timing::factory()->create();
+
+        // new data
+        $timeFrame = $this->faker->lexify('???');
+
+        $response = $this->patch(route('timing.update', [$timing->id]), [
+            'timeFrame' => $timeFrame,
+        ]);
+
+        $this->assertEquals($timeFrame, Timing::first()->timeFrame);
+        $response->assertRedirect($timing->path());
+    }
+
+    /**
+     * test timing can be deleted.
+     * 
+     * @return void
+     */
+    public function testDeleteTiming()
+    {
+        $this->withoutExceptionHandling();
+        
+        $timing = Timing::factory()->create();
+
+        $this->assertDatabaseCount($timing->getTable(), 1);
+
+        $this->delete(route('timing.destroy', [$timing->id]));
+
+        $this->assertDeleted($timing);
+    }
 }
