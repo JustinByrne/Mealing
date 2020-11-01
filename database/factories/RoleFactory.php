4cf8351f184e2ccb6c9e5be7a79cca147a99b204
Justin Byrne <theskaterjesus@gmail.com>
2020-11-01 21:40:06 +0000

    Created Role factory

---
+++ database/factories/RoleFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Role;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class RoleFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Role::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->lexify('???'),
+        ];
+    }
+}
