f410756553487e289a71f43ac39fd122645f81d6
Justin Byrne <theskaterjesus@gmail.com>
2020-11-03 22:39:15 +0000

    Created Ingredient store, update, destroy function

--- app/Http/Controllers/IngredientController.php
+++ app/Http/Controllers/IngredientController.php
@@ -3,8 +3,47 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\StoreIngredientRequest;
+use App\Http\Requests\UpdateIngredientRequest;
+use App\Models\Ingredient;
 
 class IngredientController extends Controller
 {
-    //
+    /**
+     * Create new ingredient
+     * 
+     * @param App\Http\Requests\StoreIngredientRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreIngredientRequest $request)
+    {
+        $ingredient = Ingredient::create($request->validated());
+
+        return redirect($ingredient->path());
+    }
+
+    /**
+     * Update ingredient
+     * 
+     * @param App\Http\Requests\UpdateIngredientRequest $request
+     * @param App\Models\Ingredient $ingredient
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateIngredientRequest $request, Ingredient $ingredient)
+    {
+        $ingredient->update($request->validated());
+
+        return redirect($ingredient->path());
+    }
+
+    /**
+     * Delete ingredient
+     * 
+     * @param App\Models\Ingredient $ingredient
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Ingredient $ingredient)
+    {
+        $ingredient->delete();
+    }
 }
