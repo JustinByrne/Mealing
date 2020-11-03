37b71a2f5fa4a2b461656911ae74bddc03c226ea
Justin Byrne <theskaterjesus@gmail.com>
2020-11-03 23:09:33 +0000

    removed nullable from roles table

--- database/migrations/2020_11_01_212244_create_roles_table.php
+++ database/migrations/2020_11_01_212244_create_roles_table.php
@@ -15,7 +15,7 @@ class CreateRolesTable extends Migration
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
-            $table->string('title')->nullable();
+            $table->string('title');
             $table->timestamps();
             $table->softDeletes();
         });
