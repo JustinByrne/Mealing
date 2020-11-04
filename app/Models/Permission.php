bb3a8084f2169f852853bf77028c2b5a981ba833
Justin Byrne <theskaterjesus@gmail.com>
2020-11-04 08:38:05 +0000

    Creted Permission model

---
+++ app/Models/Permission.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Permission extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title',
+    ];
+
+    /**
+     * Get the url path for the Meal
+     */
+    public function path()
+    {
+        return route('permissions.show', [$this->id]);
+    }
+}
