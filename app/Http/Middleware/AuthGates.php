b577dd7f7f3f2956da49357c13fa9ad3d3e66c5e
Justin Byrne <theskaterjesus@gmail.com>
2020-11-05 16:03:40 +0000

    Created middleware to define gate policies

---
+++ app/Http/Middleware/AuthGates.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facade\Gate;
+use App\Models\Role;
+use App\Models\User;
+
+class AuthGates
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $user = \Auth::user();
+
+        if (!app()->runningInConsole() && $user)  {
+            $roles = Role::with('permissions')->get();
+            $permissionsArray = [];
+
+            foreach ($roles as $role)  {
+                foreach ($role->permissions as $permissions)    {
+                    $permissionsArray[$permissions->title][] = $role->id;
+                }
+            }
+
+            foreach ($permissionsArray as $title => $role)  {
+                Gate::define($title, function (User $user) use ($roles) {
+                    return count(array_intersect($user->roles->pluck('id')->toArray(), $roles)) > 0;
+                });
+            }
+        }
+        
+        return $next($request);
+    }
+}
