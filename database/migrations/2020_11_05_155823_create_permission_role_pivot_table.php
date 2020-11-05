92355f04f2e88fae1a2e0c4d88da0ce8f01fb188
Justin Byrne <theskaterjesus@gmail.com>
2020-11-05 16:00:50 +0000

    Create a Permission Role pivot table

---
+++ database/migrations/2020_11_05_155823_create_permission_role_pivot_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePermissionRolePivotTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('permission_role', function (Blueprint $table) {
+            $table->foreignId('permission_id')->constant();
+            $table->foreignId('role_id')->constant();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('permission_role');
+    }
+}
