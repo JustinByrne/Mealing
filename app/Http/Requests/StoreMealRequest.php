d97a0f659b7b388bf42cd95bfba61fee3d9f4bda
Justin Byrne <theskaterjesus@gmail.com>
2020-11-05 23:15:28 +0000

    Added gate to protect validation

--- app/Http/Requests/StoreMealRequest.php
+++ app/Http/Requests/StoreMealRequest.php
@@ -3,6 +3,8 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Symfony\Component\HttpFoundation\Response;
+use Gate;
 
 class StoreMealRequest extends FormRequest
 {
@@ -13,6 +15,8 @@ class StoreMealRequest extends FormRequest
      */
     public function authorize()
     {
+        abort_if(Gate::denies('meal_create'), Response::HTTP_FORBIDDEN, '403 Forbidden');
+        
         return true;
     }
 
