cee9c8df860e5ba8e73c97b9a3414ac22c2c7b6d
Justin Byrne <theskaterjesus@gmail.com>
2020-11-08 13:57:14 +0000

    Added Serving index,create,show,edit tests & func

--- app/Http/Controllers/ServingController.php
+++ app/Http/Controllers/ServingController.php
@@ -12,6 +12,28 @@ use Gate;
 class ServingController extends Controller
 {
     /**
+     * Show all the servings
+     * 
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        abort_if(Gate::denies('serving_access'), Response::HTTP_FORBIDDEN, '403 Forbidden');
+
+        $servings = Serving::all();
+    }
+
+    /**
+     * Show the form to create a serving
+     * 
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        abort_if(Gate::denies('serving_create'), Response::HTTP_FORBIDDEN, '403 Forbidden');
+    }
+    
+    /**
      * Create new Serving
      * 
      * @param \App\Http\Requests\StoreServingRequest  $request
@@ -22,6 +44,28 @@ class ServingController extends Controller
         $serving = Serving::create($request->validated());
 
         return redirect($serving->path());
+    }
+
+    /**
+     * Show all the individual serving
+     * 
+     * @param \App\Models\Serving $serving
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Serving $serving)
+    {
+        abort_if(Gate::denies('serving_show'), Response::HTTP_FORBIDDEN, '403 Forbidden');
+    }
+
+    /**
+     * Show the form to edit a serving
+     * 
+     * @param \App\Models\Serving $serving
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Serving $serving)
+    {
+        abort_if(Gate::denies('serving_edit'), Response::HTTP_FORBIDDEN, '403 Forbidden');
     }
 
     /**
--- tests/Feature/ServingTest.php
+++ tests/Feature/ServingTest.php
@@ -15,6 +15,72 @@ class ServingTest extends TestCase
     use WithFaker;
 
     /**
+     * test the index of servings.
+     * 
+     * @return void
+     */
+    public function testServingIndex()
+    {
+        $this->withoutExceptionHandling();
+
+        $this->seed();
+        $user = User::factory()->create();
+        $adminId = Role::find(1)->id;
+        $user->roles()->sync([$adminId]);
+        
+        $response = $this->actingAs($user)->get(route('servings.index'));
+
+        $response->assertOk();
+    }
+
+    /**
+     * test the index of servings without permission.
+     * 
+     * @return void
+     */
+    public function testServingIndexWithoutPermission()
+    {
+        $response = $this->get(route('servings.index'));
+
+        $response->assertStatus(403);
+    }
+
+    /**
+     * test serving create form.
+     * 
+     * @return void
+     */
+    public function testServingCreateForm()
+    {
+        $this->withoutExceptionHandling();
+
+        $this->seed();
+        $user = User::factory()->create();
+        $adminId = Role::find(1)->id;
+        $user->roles()->sync([$adminId]);
+
+        $serving = Serving::factory()->create();
+
+        $response = $this->actingAs($user)->get(route('servings.create', [$serving->id]));
+
+        $response->assertOk();
+    }
+
+    /**
+     * test serving create form without permission.
+     * 
+     * @return void
+     */
+    public function testServingCreateFormWithoutPermission()
+    {
+        $serving = Serving::factory()->create();
+
+        $response = $this->get(route('servings.create', [$serving->id]));
+
+        $response->assertStatus(403);
+    }
+
+    /**
      * test create new serving.
      *
      * @return void
@@ -73,6 +139,79 @@ class ServingTest extends TestCase
             ->post(route('servings.store'), [
                 'quantity' => $this->faker->lexify('???'),
         ]);
+
+        $response->assertStatus(403);
+    }
+
+    /**
+     * test a serving can be shown.
+     * 
+     * @return void
+     */
+    public function testShowingserving()
+    {
+        $this->withoutExceptionHandling();
+
+        $this->seed();
+        $user = User::factory()->create();
+        $adminId = Role::find(1)->id;
+        $user->roles()->sync([$adminId]);
+
+        $serving = Serving::factory()->create();
+
+        $response = $this->actingAs($user)->get(route('servings.show', [$serving->id]));
+
+        $response->assertOk();
+    }
+
+    /**
+     * test a serving can be shown without permission.
+     * 
+     * @return void
+     */
+    public function testShowingservingWithoutPermission()
+    {
+        $this->seed();
+        $user = User::factory()->create();
+
+        $serving = Serving::factory()->create();
+
+        $response = $this->actingAs($user)->get(route('servings.show', [$serving->id]));
+
+        $response->assertStatus(403);
+    }
+
+    /**
+     * test serving edit form.
+     * 
+     * @return void
+     */
+    public function testservingEditForm()
+    {
+        $this->withoutExceptionHandling();
+
+        $this->seed();
+        $user = User::factory()->create();
+        $adminId = Role::find(1)->id;
+        $user->roles()->sync([$adminId]);
+
+        $serving = Serving::factory()->create();
+
+        $response = $this->actingAs($user)->get(route('servings.edit', [$serving->id]));
+
+        $response->assertOk();
+    }
+
+    /**
+     * test serving edit form without permission.
+     * 
+     * @return void
+     */
+    public function testservingEditFormWithoutPermission()
+    {
+        $serving = Serving::factory()->create();
+
+        $response = $this->get(route('servings.edit', [$serving->id]));
 
         $response->assertStatus(403);
     }
