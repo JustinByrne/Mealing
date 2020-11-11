361c3348f0bb074f2f2537f9894e198306c1ab13
Justin Byrne <theskaterjesus@gmail.com>
2020-11-11 13:34:37 +0000

    Created register and login requests

---
+++ app/Http/Requests/StoreRegisterRequest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreRegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required',
+            'email' => 'required|unique:App\Models\User,email',
+            'password' => 'required|confirmed'
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'name.required' => 'A name is required',
+            'email.required' => 'An email is required',
+            'email.unique' => 'This email has already been used',
+            'password.required' => 'A password is required',
+            'password.confirmed' => 'The passwords do not match',
+        ];
+    }
+}
