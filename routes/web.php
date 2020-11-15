040328d5ef3a4e7cdca5d6e2262b6fafa59016af
Justin Byrne <theskaterjesus@gmail.com>
2020-11-15 23:55:28 +0000

    Changed home page to landing page

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ use App\Http\Controllers\AuthController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('landing');
 });
 
 Route::get('/register', [AuthController::class, 'register'])->name('register');
