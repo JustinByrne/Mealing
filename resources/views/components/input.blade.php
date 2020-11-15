0b5fd7e2beb2ad47102295677809f162b8b5315f
Justin Byrne <theskaterjesus@gmail.com>
2020-11-15 23:56:25 +0000

    Added error processing to input

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -1,3 +1,9 @@
-@props(['disabled' => false])
+@props(['disabled' => false, 'error'])
 
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500']) !!}>
+@php
+    $classes = ($error ?? false)
+                ? 'bg-red-200 appearance-none border-2 border-red-400 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-red-500'
+                : 'bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500'
+@endphp
+
+<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => $classes]) !!}>
