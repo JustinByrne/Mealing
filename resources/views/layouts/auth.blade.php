2cde2809fb41fecfaae1e1c616888bf1bba129d8
Justin Byrne <theskaterjesus@gmail.com>
2020-11-15 23:16:41 +0000

    Created auth layout

---
+++ resources/views/layouts/auth.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title>Mealing | @yield('title')</title>
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    </head>
+
+    <body class="antialiased bg-gray-200">
+        @yield('content')
+    </body>
+</html>
