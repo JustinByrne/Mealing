94847dd20a047e32f1c0562de5f52dadd6bd0782
Justin Byrne <theskaterjesus@gmail.com>
2020-11-16 23:43:02 +0000

    Changed auth card to change where heading is

--- resources/views/components/auth-card.blade.php
+++ resources/views/components/auth-card.blade.php
@@ -1,9 +1,6 @@
 <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0">
-    <div>
-        Mealing
-    </div>
-
-    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+    <div class="w-full sm:max-w-md px-6 py-4 bg-white shadow-lg overflow-hidden sm:rounded-lg">
+        <h1 class="text-center text-xl pb-8">Mealing</h1>
         {{ $slot }}
     </div>
 </div>
