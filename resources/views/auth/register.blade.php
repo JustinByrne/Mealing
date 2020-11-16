5ba9f9919f7e7f924b1b2951e2e90e1cbcf0a8cb
Justin Byrne <theskaterjesus@gmail.com>
2020-11-16 23:50:37 +0000

    Finished regsitration form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,6 +4,9 @@
 
 @section('content')
 <x-auth-card>
+    <p class="text-red-500 text-sm text-center font-bold pb-2">
+        {{ $errors->first('failed') }}
+    </p>
     <form class="w-full max-w-sm" method="POST" action="{{ route('register.store') }}">
         @csrf
         <div class="md:flex md:items-center mb-6">
@@ -13,7 +16,10 @@
                 </label>
             </div>
             <div class="md:w-2/3">
-                <x-input name="name" type="text" placeholder="Name" :error="$errors->has('name')"></x-input>
+                <x-input name="name" type="text" placeholder="Name" :error="$errors->has('name')" value="{{ Request::old('name') }}" required></x-input>
+                <p class="text-red-500 text-xs italic">
+                    {{ $errors->first('name') }}
+                </p>
             </div>
         </div>
         <div class="md:flex md:items-center mb-6">
@@ -23,7 +29,10 @@
                 </label>
             </div>
             <div class="md:w-2/3">
-                <x-input name="email" type="email" placeholder="Email" :error="$errors->has('email')"></x-input>
+                <x-input name="email" type="email" placeholder="Email" :error="$errors->has('email')" value="{{ Request::old('email') }}" required></x-input>
+                <p class="text-red-500 text-xs italic">
+                    {{ $errors->first('email') }}
+                </p>
             </div>
         </div>
         <div class="md:flex md:items-center mb-6">
@@ -33,7 +42,10 @@
                 </label>
             </div>
             <div class="md:w-2/3">
-                <x-input name="password" type="password" placeholder="******************" :error="$errors->has('password')"></x-input>
+                <x-input name="password" type="password" placeholder="******************" :error="$errors->has('password')" required></x-input>
+                <p class="text-red-500 text-xs italic">
+                    {{ $errors->first('password') }}
+                </p>
             </div>
         </div>
         <div class="md:flex md:items-center mb-6">
@@ -43,15 +55,18 @@
                 </label>
             </div>
             <div class="md:w-2/3">
-                <x-input name="password_confirmation" type="password" placeholder="******************" :error="$errors->has('password')"></x-input>
+                <x-input name="password_confirmation" type="password" placeholder="******************" :error="$errors->has('password')" required></x-input>
             </div>
         </div>
         <div class="md:flex md:items-center">
             <div class="md:w-1/3"></div>
             <div class="md:w-2/3">
                 <x-button type="submit">
-                    Register
+                    {{ __('Register') }}
                 </x-button>
+                <a href="{{ route('login') }}" class="pl-4 text-blue-600 font-bold hover:text-blue-400">
+                    {{ __('Login') }}
+                </a>
             </div>
         </div>
     </form>
