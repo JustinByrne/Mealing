18a3c61e59c7fd6d205320af513aae81687db59a
Justin Byrne <theskaterjesus@gmail.com>
2020-11-17 22:47:30 +0000

    Only showing title if present

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <title>Mealing | @yield('title')</title>
+        <title>Mealing @if (trim($__env->yieldContent('title'))) | @yield('title') @endif</title>
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
         <style>
             #menu-toggle:checked + #menu {
