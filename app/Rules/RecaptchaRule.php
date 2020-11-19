b92916a12348ffdf3bcd052309880ea5d885a977
Justin Byrne <theskaterjesus@gmail.com>
2020-11-19 00:14:16 +0000

    Created a ReCaptcha validation rule

---
+++ app/Rules/RecaptchaRule.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+use ReCaptcha\ReCaptcha;
+
+class RecaptchaRule implements Rule
+{
+    private $errors;
+
+    /**
+     * Create a new rule instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        if (env('RECAPTCHA_PHPUNIT_TESTS'))   {
+            return true;
+        }
+        
+        $rScore = 0.5;
+        
+        if (empty($value))  {
+            $this->errors = ':atrribute field is required';
+
+            return false;
+        }
+
+        $recaptcha = new Recaptcha(env('RECAPTCHA_SECRET_KEY'));
+
+        $resp = $recaptcha->setExpectedHostname($_SERVER['HTTP_HOST'])
+                  ->setScoreThreshold($rScore)
+                  ->verify($value, $_SERVER['REMOTE_ADDR']);
+        
+        if (!$resp->isSuccess())    {
+            $this->errors = $resp->getErrorCodes();
+
+            return false;
+        }
+
+        if ($resp->getScore() < $rScore)    {
+            $this->errors = 'Failed to valildate ReCaptcha';
+
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return $this->errors;
+    }
+}
