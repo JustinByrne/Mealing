130c97232bd0574824745a575d0e86aebbf41189
Justin Byrne <theskaterjesus@gmail.com>
2020-11-19 23:06:00 +0000

    updated remember me design

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -37,14 +37,14 @@
             </div>
         </div>
         <div class="md:flex md:items-center mb-6">
-            <div class="md:w-1/3 text-right"></div>
-            <div class="md:w-2/3">
-                <input class="mr-3" name="remember" type="checkbox">
-                <label class="text-gray-500 font-bold mb-1 md:mb-0 pr-4" for="password">
-                    Remember Me
-                </label>
-            </div>
-        </div>
+            <div class="md:w-1/3"></div>
+            <label class="md:w-2/3 block text-gray-500 font-bold">
+              <input class="mr-2 leading-tight" type="checkbox" name="remember">
+              <span class="text-sm">
+                Remember Me
+              </span>
+            </label>
+          </div>
         <div class="md:flex md:items-center">
             <div class="md:w-1/3"></div>
             <div class="md:w-2/3">
