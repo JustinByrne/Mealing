f16ee8a1cf5d59ec73deda5a7cca7b89a81fbb44
Justin Byrne <theskaterjesus@gmail.com>
2020-11-20 20:06:06 +0000

    Replaced css with jquery

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,15 +5,13 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <title>Mealing @if (trim($__env->yieldContent('title'))) | @yield('title') @endif</title>
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-        <style>
-            #menu-toggle:checked + #menu {
-                display: block;
-            }
-        </style>
+        <script src="https://kit.fontawesome.com/a357069ed8.js" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     </head>
 
     <body class="antialiased bg-gray-900">
         @include('layouts.navigation')
         @yield('content')
+        @yield('scripts')
     </body>
 </html>
