87993f101081077553c4a1c809d4d51de2726fc0
Justin Byrne <theskaterjesus@gmail.com>
2020-11-23 22:17:01 +0000

    Created sidebar link component

---
+++ resources/views/components/side-link.blade.php
@@ -0,0 +1,5 @@
+<li class="py-2 md:my-0">
+    <a {{ $attributes->merge() }} class="block pl-4 align-middle text-white no-underline hover:text-orange-300 border-l-4 border-transparent hover:border-orange-400">
+        <span class="pb-1 md:pb-0 text-sm">{{ $slot }}</span>
+    </a>
+</li>
