9c0e721026abf24393d1d5ecf99d4d2458f360ef
Justin Byrne <theskaterjesus@gmail.com>
2020-11-23 22:18:18 +0000

    Updated layout to include a sidebar menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,8 +4,22 @@
         @include('layouts.head')
     </head>
 
-    <body class="antialiased bg-blueGray-800">
+    <body class="antialiased bg-blueGray-600">
         @include('layouts.navigation')
-        @yield('content')
+        <div class="container w-full flex flex-wrap mx-auto mt-12">
+            <div class="w-full lg:w-1/5 lg:px-6 text-xl text-gray-800 leading-normal">    
+                <p class="text-base font-bold py-2 lg:pb-6 text-white">
+                    @if (trim($__env->yieldContent('side-links'))) Menu @endif
+                </p>
+                <div class="w-full sticky inset-0 overflow-x-hidden overflow-y-auto mt-0 z-20 lg:overflow-y-hidden lg:block">
+                    <ul class="list-reset">
+                        @yield('side-links')
+                    </ul>
+                </div>
+            </div>
+            <div class="w-full p-8 mt-6 text-gray-900 leading-normal shadow-lg bg-white rounded lg:mt-0 lg:w-4/5">
+                @yield('content')
+            </div>
+        </div>
     </body>
 </html>
