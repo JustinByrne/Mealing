f99fad2cc0eb358112d556b420a139d252b70f67
Justin Byrne <theskaterjesus@gmail.com>
2020-11-25 23:52:14 +0000

    Added page to show all ingredients

--- resources/views/ingredients/index.blade.php
+++ resources/views/ingredients/index.blade.php
@@ -1,3 +1,50 @@
 @extends('layouts.app')
 
-@section('title', 'Meals')
+@section('title', 'Ingredients')
+
+@section('side-links')
+    <x-side-link href="{{ route('ingredients.create') }}">Create new Ingredient</x-side-link>
+@endsection
+
+@section('content')
+    <div class="font-sans">
+        <h1 class="font-sans break-normal text-gray-900 pt-6 pb-2 text-xl">Ingredients</h1>
+        <hr class="border-b border-gray-400">
+    </div>
+
+    <div class="flex flex-col mt-5">
+        <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                    <table class="min-w-full divide-y divide-gray-200">
+                        <thead>
+                            <tr>
+                                <x-th>Ingredient</x-th>
+                                <x-th>Number of Meals</x-th>
+                                <x-th></x-th>
+                            </tr>
+                        </thead>
+                        <tbody class="bg-white divide-y divide-gray-200">
+                            @foreach ($ingredients as $ingredient)
+                                <tr>
+                                    <x-td>{{ $ingredient->name }}</x-td>
+                                    <x-td>{{ $ingredient->meals()->count() }}</x-td>
+                                    <x-td class="text-right font-medium">
+                                        <div class="inline-flex">
+                                            <a href="{{ $ingredient->path() }}/edit" class="px-2 py-1 text-blueGray-600 rounded-full  hover:text-white hover:bg-green-600">Edit</a>
+                                            <form action="{{ $ingredient->path() }}" method="POST">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button class="px-2 py-1 text-blueGray-600 rounded-full font-medium hover:text-white hover:bg-red-600">Delete</button>
+                                            </form>
+                                        </div>
+                                    </x-td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
