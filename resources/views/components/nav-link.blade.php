210a37bed61569a4d10f3c921cc3a3b935a48922
Justin Byrne <theskaterjesus@gmail.com>
2020-11-26 23:21:50 +0000

    Updated nav link design slightly

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -2,7 +2,7 @@
 
 @php
     $classes = ($active ?? false)
-        ? 'px-3 py-5 text-gray-400 border-b-2 border-transparent bg-gray-900 hover:text-orange-300 hover:border-orange-300 lg:inline-flex lg:w-auto'
+        ? 'px-3 py-5 text-gray-400 border-b-2 bg-gray-900 hover:text-orange-300 border-orange-300 lg:inline-flex lg:w-auto'
         : 'px-3 py-5 text-gray-400 border-b-2 border-transparent hover:bg-gray-900 hover:text-orange-300 hover:border-orange-300 lg:inline-flex lg:w-auto'
 @endphp
 
