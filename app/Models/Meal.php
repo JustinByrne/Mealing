a46de1e61838e4cf89f13c5612f7ef2986d393ea
Justin Byrne <theskaterjesus@gmail.com>
2020-12-03 15:54:08 +0000

    Added a quantity for ingredients for meals

--- app/Models/Meal.php
+++ app/Models/Meal.php
@@ -29,7 +29,7 @@ class Meal extends Model
      */
     public function ingredients()
     {
-        return $this->belongsToMany('App\Models\Ingredient');
+        return $this->belongsToMany('App\Models\Ingredient')->withPivot('quantity');
     }
 
     /**
