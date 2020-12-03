fc654b76b766eeb0b1a8f4c62f82d9b54d4c959a
Justin Byrne <theskaterjesus@gmail.com>
2020-12-03 14:45:52 +0000

    Added instructions for Meals

---
+++ database/migrations/2020_12_03_144041_add_instructions_to_meals_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddInstructionsToMealsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('meals', function (Blueprint $table) {
+            $table->text('instruction')->nullable()->after('timing');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('meals', function (Blueprint $table) {
+            //
+        });
+    }
+}
