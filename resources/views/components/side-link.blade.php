9298064299803a02156ffaf7939c0bf60bba3435
Justin Byrne <theskaterjesus@gmail.com>
2020-12-05 14:58:23 +0000

    Made the sidebar links bold

--- resources/views/components/side-link.blade.php
+++ resources/views/components/side-link.blade.php
@@ -2,8 +2,8 @@
 
 @php
     $classes = ($active ?? false)
-        ? 'block pl-4 align-middle text-white no-underline border-l-4 border-orange-400 hover:text-orange-300'
-        : 'block pl-4 align-middle text-white no-underline border-l-4 border-transparent hover:border-orange-400 hover:text-orange-300'
+        ? 'block pl-4 align-middle text-white no-underline border-l-4 border-orange-400 font-bold hover:text-orange-300'
+        : 'block pl-4 align-middle text-white no-underline border-l-4 border-transparent font-bold hover:border-orange-400 hover:text-orange-300'
 @endphp
 
 <li class="py-2 md:my-0">
