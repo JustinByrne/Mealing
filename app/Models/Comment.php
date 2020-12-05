0df02e85bd7514c4d45512633e931dcc38afd6f6
Justin Byrne <theskaterjesus@gmail.com>
2020-12-05 15:18:53 +0000

    Added user to comment model

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     use HasFactory;
+
+    /**
+     * The User that owns the Comment
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
