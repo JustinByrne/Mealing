0d6e3533f5bfeedac72c6b4e8e613d7b4f389e96
Justin Byrne <theskaterjesus@gmail.com>
2020-12-06 23:03:58 +0000

    Added fillable and human readable date

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -10,10 +10,30 @@ class Comment extends Model
     use HasFactory;
 
     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'comment',
+        'user_id',
+    ];
+
+    /**
      * The User that owns the Comment
      */
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Changing the created at date to human readable
+     * 
+     * @return \Carbon\CarbonInterval
+     */
+    public function getCreatedAtAttribute($value)
+    {
+        return \Carbon\Carbon::parse($value)->format('j<\s\up>S</\s\up> F Y');
     }
 }
