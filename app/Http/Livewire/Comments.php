ac551f2547fae79d47d7bff272bce5cb00668b90
Justin Byrne <theskaterjesus@gmail.com>
2020-12-06 23:04:29 +0000

    Added ability to add comments for meal

--- app/Http/Livewire/Comments.php
+++ app/Http/Livewire/Comments.php
@@ -8,11 +8,26 @@ use App\Models\Meal;
 class Comments extends Component
 {
     public Meal $meal;
+    public $comment;
+
+    protected $rules = [
+        'comment' => 'required'
+    ];
     
     public function render()
     {
         return view('livewire.comments', [
             'meal' => $this->meal,
+        ]);
+    }
+
+    public function addComment()
+    {
+        $this->validate();
+
+        $this->meal->comments()->create([
+            'comment' => $this->comment,
+            'user_id' => \Auth::Id(),
         ]);
     }
 }
