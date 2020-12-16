0c7883078c1e2fc3bcbb939e58458e294b58a432
Justin Byrne <theskaterjesus@gmail.com>
2020-12-16 14:38:20 +0000

    Added a modal to show QR code for mfa

--- resources/views/user/profile/security.blade.php
+++ resources/views/user/profile/security.blade.php
@@ -8,7 +8,7 @@
     <h1 class="font-sans break-normal text-gray-900 pt-6 pb-4 text-xl">Security Settings</h1>
     <hr class="border-b border-gray-400 mb-3">
 
-    <h2 class="font-sans break-normal text-gray-900 pt-6 pb-4 text-lg font-bold">Multifactor Authentication</h2>
+    <h2 class="font-sans break-normal text-gray-900 pt-3 pb-3 text-base font-bold">Multifactor Authentication</h2>
 
     @if (session('status') == 'two-factor-authentication-enabled')
         <x-alert type="info">
@@ -21,23 +21,21 @@
             Two factor authentication has been disabled.
         </x-alert>
     @endif
-
-    <div x-data="{ open: false }">
-        @if ($user->get2faEnabled())
-            <a href="#" @click="open = true" x-on:click.prevent class="hover:text-orange-400 hover:underline">Show QR Code</a>
-            <div class="absolute top-0 left-0 flex items-center justify-center w-full h-full" style="background-color: rgba(0,0,0,.5);" x-show="open">
-            <div class="h-auto p-4 mx-2 text-left bg-white rounded shadow-xl md:max-w-xl md:p-6 lg:p-8 md:mx-0" @click.away="open = false">
-                <div class="pb-3">
-                    {!! $user->twoFactorQrCodeSvg() !!}
-                </div>
-                <x-inputs.button @click="open = false">Close</x-inputs.button>
-            </div>
-        @endif
-    </div>
+    
 
     <form action="/user/two-factor-authentication" method="POST" class="mt-3">
         @csrf
         @if ($user->get2faEnabled())
+            <div x-data="{ open: false }">
+                <x-inputs.button @click="open = true" type="button"><i class="fas fa-qrcode"></i> Show QR Code</x-inputs.button>
+                <div class="absolute top-0 left-0 flex items-center justify-center w-full h-full" style="background-color: rgba(0,0,0,.5);" x-show="open">
+                <div class="h-auto p-4 mx-2 text-left bg-white rounded shadow-xl md:max-w-xl md:p-6 lg:p-8 md:mx-0" @click.away="open = false">
+                    <div class="pb-3">
+                        {!! $user->twoFactorQrCodeSvg() !!}
+                    </div>
+                    <x-inputs.button @click="open = false" type="button">Close</x-inputs.button>
+                </div>
+            </div>
             @method('delete')
             <x-inputs.button type="submit">Disable</x-inputs.button>
         @else
