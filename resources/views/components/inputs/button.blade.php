12468e1f6e8ed80941a5db69909ad611449b49bd
Justin Byrne <theskaterjesus@gmail.com>
2020-12-19 21:54:34 +0000

    Changed button to be mobile first

--- resources/views/components/inputs/button.blade.php
+++ resources/views/components/inputs/button.blade.php
@@ -1,3 +1,3 @@
-<button {!! $attributes->merge(['class' => 'shadow bg-blue-500 hover:bg-blue-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded']) !!}>
+<button {!! $attributes->merge(['class' => 'shadow bg-blue-500 text-white font-bold py-2 px-4 rounded w-full md:w-auto hover:bg-blue-400 focus:shadow-outline focus:outline-none']) !!}>
     {{ $slot }}
 </button>
