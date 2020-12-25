cf25f23d68660c278e4a46c87d9d4d15e2dbecdf
Justin Byrne <theskaterjesus@gmail.com>
2020-12-25 23:17:27 +0000

    Added the form to update user details

--- resources/views/user/profile/account.blade.php
+++ resources/views/user/profile/account.blade.php
@@ -8,25 +8,29 @@
     <h1 class="font-sans break-normal text-gray-900 pt-6 pb-4 text-xl">Account Settings</h1>
     <hr class="border-b border-gray-400 mb-3">
 
-    <div class="space-y-4">
-        <div class="items-center md:flex md:space-x-6">
-            <label for="name" class="font-light text-xs md:text-base">
-                Name
-            </label>
-            <x-inputs.text type="text" class="font-light text-sm md:max-w-md" name="name" id="name" value="{{ \Auth::User()->name }}" />
-        </div>
+    <form method="POST">
+        @csrf
+        @method('PATCH')
+        <div class="space-y-4">
+            <div class="items-center md:flex md:space-x-6">
+                <label for="name" class="font-light text-xs md:text-base">
+                    Name
+                </label>
+                <x-inputs.text type="text" class="font-light text-sm md:max-w-md" name="name" id="name" value="{{ \Auth::User()->name }}" />
+            </div>
 
-        <div class="items-center md:flex md:space-x-6">
-            <label for="email" class="font-light text-xs md:text-base">
-                Email
-            </label>
-            <x-inputs.text type="email" class="font-light text-sm md:max-w-md" name="email" id="email" value="{{ \Auth::User()->email }}" />
+            <div class="items-center md:flex md:space-x-6">
+                <label for="email" class="font-light text-xs md:text-base">
+                    Email
+                </label>
+                <x-inputs.text type="email" class="font-light text-sm md:max-w-md" name="email" id="email" value="{{ \Auth::User()->email }}" />
+            </div>
+            
+            <div>
+                <x-inputs.button type="submit">
+                    Save
+                </x-inputs.button>
+            </div>
         </div>
-        
-        <div>
-            <x-inputs.button type="submit">
-                Save
-            </x-inputs.button>
-        </div>
-    </div>
+    </form>
 @endsection
