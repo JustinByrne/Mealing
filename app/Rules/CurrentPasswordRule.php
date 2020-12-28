7f2a7e7c40a97e146896985e128c2565e0f21333
Justin Byrne <theskaterjesus@gmail.com>
2020-12-28 20:07:06 +0000

    Added a validation rule to check user password

---
+++ app/Rules/CurrentPasswordRule.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Rules;
+
+use Auth;
+use Illuminate\Contracts\Validation\Rule;
+
+class CurrentPasswordRule implements Rule
+{
+    /**
+     * Create a new rule instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        if (!Hash::check($value, Auth::User()->password))   {
+            return false;
+        }
+        
+        return true;
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return 'Users details don\'t match our records.';
+    }
+}
