13e2a1a274e4f355f41b6c87a5d24131d94a2b4e
Justin Byrne <theskaterjesus@gmail.com>
2020-12-29 23:41:15 +0000

    Added extra margin to buttons

--- resources/views/components/inputs/button.blade.php
+++ resources/views/components/inputs/button.blade.php
@@ -1,3 +1,3 @@
-<button {!! $attributes->merge(['class' => 'shadow bg-blue-500 text-white font-bold py-2 px-4 rounded w-full md:w-auto hover:bg-blue-400']) !!}>
+<button {!! $attributes->merge(['class' => 'shadow bg-blue-500 text-white font-bold py-2 px-4 mb-2 rounded w-full md:w-auto hover:bg-blue-400']) !!}>
     {{ $slot }}
 </button>
