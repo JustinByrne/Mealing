d0c89fdf59fc6743a9708620fa1fe7dcb85a93ae
Justin Byrne <theskaterjesus@gmail.com>
2020-12-30 19:39:57 +0000

    Changed User factory to FreeEmail This was to help with dns Validation

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,7 +24,7 @@ class UserFactory extends Factory
     {
         return [
             'name' => $this->faker->name,
-            'email' => $this->faker->unique()->safeEmail,
+            'email' => $this->faker->unique()->freeEmail,
             'email_verified_at' => now(),
             'password' => 'password',
             'remember_token' => Str::random(10),
