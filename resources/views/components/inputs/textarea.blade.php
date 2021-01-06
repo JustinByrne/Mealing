83efb2d7cbef764d45a059acd2293a0b62293a61
Justin Byrne <theskaterjesus@gmail.com>
2021-01-06 00:38:36 +0000

    Added textarea blade component

---
+++ resources/views/components/inputs/textarea.blade.php
@@ -0,0 +1,1 @@
+<textarea {!! $attributes->merge(['class' => 'bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full h-96 py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500']) !!}></textarea>
