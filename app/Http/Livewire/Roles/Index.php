bd4ac79f99b3422f9aa7242769dd9fc217d5108d
Justin Byrne <theskaterjesus@gmail.com>
2021-01-10 22:36:04 +0000

    Added livewire search for roles

--- app/Http/Livewire/Roles/Index.php
+++ app/Http/Livewire/Roles/Index.php
@@ -7,9 +7,16 @@ use Livewire\Component;
 
 class Index extends Component
 {
+    public $search;
+    
+    /**
+     * function to render the livewire view
+     * 
+     * @return Illuminate\View\View
+     */
     public function render()
     {
-        $roles = Role::with('permissions')->with('users')->get();
+        $roles = Role::with('permissions')->with('users')->where('title', 'like', '%' . $this->search . '%')->get();
         
         return view('livewire.roles.index', compact('roles'));
     }
