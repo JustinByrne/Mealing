b192e0ca6c2826c1d5941220e96b7a0b2ee546e1
Justin Byrne <theskaterjesus@gmail.com>
2021-01-14 22:30:21 +0000

    updated textarea to include data inside

--- resources/views/components/inputs/textarea.blade.php
+++ resources/views/components/inputs/textarea.blade.php
@@ -1,1 +1,1 @@
-<textarea {!! $attributes->merge(['class' => 'bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500']) !!}></textarea>
+<textarea {!! $attributes->merge(['class' => 'bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500']) !!}>{{ $slot }}</textarea>
