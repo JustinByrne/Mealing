0a69bd20142f631074a4075ef618ac736da2d371
Justin Byrne <theskaterjesus@gmail.com>
2021-01-17 13:00:12 +0000

    Added permissions links to admin menu

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -9,4 +9,14 @@
             </x-links.sidebar>
         </div>
     </div>
+    <div>
+        <x-links.sidebar href="{{ route('admin.permissions.index') }}" :active="request()->routeIs('admin.permissions*')">
+            Permissions
+        </x-links.sidebar>
+        <div>
+            <x-links.sidebar href="{{ route('admin.permissions.create') }}" :active="request()->routeIs('admin.permissions.create')">
+                <span class="font-bold mr-2">-</span> Create New Permission
+            </x-links.sidebar>
+        </div>
+    </div>
 @endsection
