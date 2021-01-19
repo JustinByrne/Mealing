7d6af0e161afc5ff7c880995fe663a6f1de7c141
Justin Byrne <theskaterjesus@gmail.com>
2021-01-19 20:12:46 +0000

    Added merge attribute to badges

--- resources/views/components/badge.blade.php
+++ resources/views/components/badge.blade.php
@@ -2,37 +2,37 @@
 
 @switch($color)
     @case('gray')
-        <span class="bg-gray-100 py-1 px-3.5 text-blueGray-700 font-medium text-sm rounded-full">        
+        <span {{ $attributes->merge(['class' => 'bg-gray-100 py-1 px-3.5 text-blueGray-700 font-medium text-sm rounded-full"']) }}>
         @break
     @case('red')
-        <span class="bg-red-100 py-1 px-3.5 text-red-800 font-medium text-sm rounded-full">
+        <span {{ $attributes->merge(['class' => 'bg-red-100 py-1 px-3.5 text-red-800 font-medium text-sm rounded-full']) }}>
         @break
     @case('orange')
-        <span class="bg-orange-100 py-1 px-3.5 text-orange-800 font-medium text-sm rounded-full">
+        <span {{ $attributes->merge(['class' => 'bg-orange-100 py-1 px-3.5 text-orange-800 font-medium text-sm rounded-full']) }}>
         @break
     @case('yellow')
-        <span class="bg-yellow-100 py-1 px-3.5 text-yellow-800 font-medium text-sm rounded-full">
+        <span {{ $attributes->merge(['class' => 'bg-yellow-100 py-1 px-3.5 text-yellow-800 font-medium text-sm rounded-full']) }}>
         @break
     @case('green')
-        <span class="bg-green-100 py-1 px-3.5 text-green-800 font-medium text-sm rounded-full">
+        <span {{ $attributes->merge(['class' => 'bg-green-100 py-1 px-3.5 text-green-800 font-medium text-sm rounded-full']) }}>
         @break
     @case('teal')
-        <span class="bg-teal-100 py-1 px-3.5 text-teal-800 font-medium text-sm rounded-full">
+        <span {{ $attributes->merge(['class' => 'bg-teal-100 py-1 px-3.5 text-teal-800 font-medium text-sm rounded-full']) }}>
         @break
     @case('lightBlue')
-        <span class="bg-lightBlue-100 py-1 px-3.5 text-lightBlue-800 font-medium text-sm rounded-full">
+        <span {{ $attributes->merge(['class' => 'bg-lightBlue-100 py-1 px-3.5 text-lightBlue-800 font-medium text-sm rounded-full']) }}>
         @break
     @case('indigo')
-        <span class="bg-indigo-100 py-1 px-3.5 text-indigo-800 font-medium text-sm rounded-full">
+        <span {{ $attributes->merge(['class' => 'bg-indigo-100 py-1 px-3.5 text-indigo-800 font-medium text-sm rounded-full']) }}>
         @break
     @case('purple')
-        <span class="bg-purple-100 py-1 px-3.5 text-purple-800 font-medium text-sm rounded-full">
+        <span {{ $attributes->merge(['class' => 'bg-purple-100 py-1 px-3.5 text-purple-800 font-medium text-sm rounded-full']) }}>
         @break
     @case('rose')
-        <span class="bg-rose-100 py-1 px-3.5 text-rose-800 font-medium text-sm rounded-full">
+        <span {{ $attributes->merge(['class' => 'bg-rose-100 py-1 px-3.5 text-rose-800 font-medium text-sm rounded-full']) }}>
         @break
     @default
-        <span class="bg-gray-100 py-1 px-3.5 text-blueGray-700 font-medium text-sm rounded-full">
+        <span {{ $attributes->merge(['class' => 'bg-gray-100 py-1 px-3.5 text-blueGray-700 font-medium text-sm rounded-full']) }}>
 @endswitch
     {{ $slot }}
 </span>
