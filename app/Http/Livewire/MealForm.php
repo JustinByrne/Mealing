3b88055e8ef5d275d3aea7ca695091dd3012d047
Justin Byrne <theskaterjesus@gmail.com>
2021-01-21 10:50:14 +0000

    Added function to add inputs to an array

--- app/Http/Livewire/MealForm.php
+++ app/Http/Livewire/MealForm.php
@@ -7,9 +7,31 @@ use App\Models\Ingredient;
 
 class MealForm extends Component
 {
+    public $quantity, $ingredient;
+    public $inputs = array();
+    public $i = 0;
+
+    /**
+     * getting the view
+     * 
+     * @return view
+     */
     public function render()
     {
         $ingredients = Ingredient::all();
         return view('livewire.meal-form', compact('ingredients'));
+    }
+
+    /**
+     * adding all input values to array
+     * 
+     * @return array
+     */
+    public function add($i)
+    {
+        $this->inputs['quantity'][$i] = $this->quantity;
+        $this->inputs['ingredient'][$i] = $this->ingredient;
+
+        $this->i = $i + 1;
     }
 }
