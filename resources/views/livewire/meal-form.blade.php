199438430b1005d2753f09035c5bd62113e18f97
Justin Byrne <theskaterjesus@gmail.com>
2021-01-26 20:34:30 +0000

    Changed the style for larger screens

--- resources/views/livewire/meal-form.blade.php
+++ resources/views/livewire/meal-form.blade.php
@@ -1,10 +1,18 @@
 <div>
+    <div class="items-top space-y-4 md:space-y-0 md:grid md:grid-cols-9 md:space-x-6">
+        <div>
+            <x-inputs.text placeholder="Amount" wire:model="quantity" />
+        </div>
+        <div class="w-full md:col-span-4">
+            <x-inputs.text placeholder="Search Ingredients..." wire:model.debounce.300ms="query" />
+        </div>
+        <div class="md:pl-6">
+            <x-inputs.button wire:click.prevent="add({{ $i }})">
+                <i class="fas fa-plus"></i>
+            </x-inputs.button>
+        </div>
+    </div>
     <div class="space-y-4">
-        <x-inputs.text placeholder="Amount" wire:model="quantity"/>
-        <x-inputs.text placeholder="Search Ingredients..." wire:model.debounce.300ms="query" />
-        <x-inputs.button wire:click.prevent="add({{ $i }})">
-            <i class="fas fa-plus"></i>
-        </x-inputs.button>
         @dump($ingredients)
     </div>
 </div>
