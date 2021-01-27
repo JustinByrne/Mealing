24dcb2343740c1a8b28158b5e3243d47918a98af
Justin Byrne <theskaterjesus@gmail.com>
2021-01-27 19:44:30 +0000

    Added dropdown for ingredients

--- resources/views/livewire/meal-form.blade.php
+++ resources/views/livewire/meal-form.blade.php
@@ -3,16 +3,39 @@
         <div>
             <x-inputs.text placeholder="Amount" wire:model="quantity" />
         </div>
-        <div class="w-full md:col-span-4">
-            <x-inputs.text placeholder="Search Ingredients..." wire:model.debounce.300ms="query" />
+        <div class="w-full relative md:col-span-4">
+            <x-inputs.text placeholder="Search Ingredients..." wire:model.debounce.300ms="query" wire:keydown.escape="resetQuery" wire:keydown.tab="resetQuery" class="z-10" />
+
+            <div class="absolute z-10 w-full bg-white rounded-b-lg shadow-lg">
+                <p wire:loading class="px-2 py-1">
+                    Searching...
+                </p>
+            </div>
+
+            @if (!empty($query))
+                <div class="absolute z-10 w-full bg-white rounded-b-lg shadow-lg">
+                    @if (!empty($ingredients))
+                        @foreach ($ingredients as $ingredient)
+                            @if ($loop->last)
+                                <p class="px-2 py-1 cursor-pointer rounded-b-lg hover:bg-blueGray-200">
+                            @else
+                                <p class="px-2 py-1 cursor-pointer hover:bg-blueGray-200">
+                            @endif
+                                {{ $ingredient['name'] }}
+                            </p>
+                        @endforeach
+                    @else
+                        <p class="px-2 py-1 cursor-pointer rounded-b-lg">
+                            No results...
+                        </p>
+                    @endif
+                </div>
+            @endif
         </div>
         <div class="md:pl-6">
             <x-inputs.button wire:click.prevent="add({{ $i }})">
                 <i class="fas fa-plus"></i>
             </x-inputs.button>
         </div>
-    </div>
-    <div class="space-y-4">
-        @dump($ingredients)
     </div>
 </div>
