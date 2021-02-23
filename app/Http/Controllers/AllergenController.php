7fc024fd49b50784ac039d812423cf32223b88ea
Justin Byrne <theskaterjesus@gmail.com>
2021-02-23 21:04:54 +0000

    Added Allergen controller and tests

---
+++ app/Http/Controllers/AllergenController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Gate;
+use App\Models\Allergen;
+use Illuminate\Http\Request;
+use App\Http\Requests\StoreAllergenRequest;
+use App\Http\Requests\UpdateAllergenRequest;
+
+class AllergenController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        abort_if(Gate::denies('allergen_access'), 403);
+
+        $allergens = Allergen::all();
+
+        return view('admin.allergens.index', compact('allergens'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        abort_if(Gate::denies('allergen_create'), 403);
+
+        return view('admin.allergens.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Request\StoreAllergenRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreAllergenRequest $request)
+    {
+        Allergen::create([
+            'name' => $request['name'],
+            'icon' => $request['icon'],
+        ]);
+
+        return redirect()->route('admin.allergens.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Allergen  $allergen
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Allergen $allergen)
+    {
+        abort_if(Gate::denies('allergen_show'), 403);
+
+        return view('admin.allergens.show', compact($allergen));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Allergen  $allergen
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Allergen $allergen)
+    {
+        abort_if(Gate::denies('allergen_edit'), 403);
+
+        return view('admin.allergens.edit', compact($allergen));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Request\UpdateAllergenRequest  $request
+     * @param  \App\Models\Allergen  $allergen
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateAllergenRequest $request, Allergen $allergen)
+    {
+        $allergen->update([
+            'name' => $request['name'],
+            'icon' => $request['icon'],
+        ]);
+
+        return redirect()->route('admin.allergens.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Allergen  $allergen
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Allergen $allergen)
+    {
+        abort_if(Gate::denies('allergen_delete'), 403);
+
+        $allergen->delete();
+
+        return redirect()->route('admin.allergens.index');
+    }
+}
---
+++ tests/Feature/AllergenTest.php
@@ -0,0 +1,450 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\Models\Role;
+use App\Models\User;
+use App\Models\Allergen;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class AllergenTest extends TestCase
+{
+    use RefreshDatabase, WithFaker;
+
+    public $user;
+
+    /**
+     * setting up a user to be used in all tests
+     * 
+     * @return void
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->seed();
+        $this->user = User::factory()->create();
+        $adminId = Role::find(1)->id;
+        $this->user->roles()->sync([$adminId]);
+    }
+    
+    /**
+     * test allowed access to allergens index
+     * 
+     * @return void
+     */
+    public function testCanAccessAllergenIndexPage()
+    {
+        $this->withoutExceptionHandling();
+        
+        $response = $this->actingAs($this->user)->get(route('admin.allergens.index'));
+
+        $response->assertOk();
+        $response->assertViewIs('admin.allergens.index');
+    }
+
+    /**
+     * test denied access to the index not logged in
+     * 
+     * @return void
+     */
+    public function testDeniedAccessToAllergenIndexPageWhenNotLoggedIn()
+    {
+        $response = $this->get(route('admin.allergens.index'));
+
+        $this->assertGuest($guard = null);
+        $response->assertRedirect(route('login'));
+    }
+
+    /**
+     * test denied access to index page when user doesn't have permission
+     * 
+     * @return void
+     */
+    public function testDeniedAccessToAllergenIndexPageWithoutPermission()
+    {
+        $user = User::factory()->create();
+        
+        $response = $this->actingAs($user)->get(route('admin.allergens.index'));
+
+        $this->assertAuthenticatedAs($user, $guard = null);
+        $response->assertForbidden();
+    }
+
+    /**
+     * test allowed access to allergen create page
+     * 
+     * @return void
+     */
+    public function testCanAccessAllergenCreatePage()
+    {
+        $this->withoutExceptionHandling();
+
+        $response = $this->actingAs($this->user)->get(route('admin.allergens.create'));
+
+        $response->assertOk();
+        $response->assertViewIs('admin.allergens.create');
+    }
+
+    /**
+     * test denied access to allergen create page when not logged in
+     * 
+     * @return void
+     */
+    public function testDeniedAccessToAllergenCreatePageWhenNotLoggedIn()
+    {
+        $response = $this->get(route('admin.allergens.create'));
+
+        $this->assertGuest($guard = null);
+        $response->assertRedirect(route('login'));
+    }
+
+    /**
+     * test denied access to create page when user doesn't have permission
+     * 
+     * @return void
+     */
+    public function testDeniedAccessToAllergenCreatePageWithoutPermission()
+    {
+        $user = User::factory()->create();
+        
+        $response = $this->actingAs($user)->get(route('admin.allergens.create'));
+
+        $this->assertAuthenticatedAs($user, $guard = null);
+        $response->assertForbidden();
+    }
+
+    /**
+     * test can create a new allergen
+     * 
+     * @return void
+     */
+    public function testCanCreateAnAllergen()
+    {
+        $this->withoutExceptionHandling();
+
+        $data = [
+            'icon' => 'eicon eicon-circle-gluten',
+            'name' => 'Gluten'
+        ];
+
+        $response = $this->actingAs($this->user)->post(route('admin.allergens.store'), $data);
+
+        $this->assertDatabaseHas(Allergen::getTableName(), $data);
+        $response->assertRedirect(route('admin.allergens.index'));
+    }
+
+    /**
+     * test denied access to create an allergen when not logged in
+     * 
+     * @return void
+     */
+    public function testDeniedAccessToCreateAllergenWhenNotLoggedIn()
+    {
+        $data = [
+            'icon' => 'eicon eicon-circle-gluten',
+            'name' => 'Gluten'
+        ];
+
+        $response = $this->post(route('admin.allergens.store'), $data);
+
+        $this->assertDatabaseMissing(Allergen::getTableName(), $data);
+        $this->assertGuest($guard = null);
+        $response->assertRedirect(route('login'));
+    }
+
+    /**
+     * test denied access to create an allergen without permission
+     * 
+     * @return void
+     */
+    public function testDeniedAccessToCreateAllergenWithoutPermission()
+    {
+        $user = User::factory()->create();
+
+        $data = [
+            'icon' => 'eicon eicon-circle-gluten',
+            'name' => 'Gluten'
+        ];
+
+        $response = $this->actingAs($user)->post(route('admin.allergens.store'), $data);
+
+        $this->assertDatabaseMissing(Allergen::getTableName(), $data);
+        $this->assertAuthenticatedAs($user, $guard = null);
+        $response->assertForbidden();
+    }
+
+    /**
+     * test error when creating an allergen without an icon
+     * 
+     * @return void
+     */
+    public function testErrorWhenCreatingAnAllergenWithoutAnIcon()
+    {
+        $data = [
+            'icon' => null,
+            'name' => 'Gluten'
+        ];
+
+        $response = $this->actingAs($this->user)->post(route('admin.allergens.store'), $data);
+
+        $this->assertDatabaseMissing(Allergen::getTableName(), $data);
+        $response->assertSessionHasErrors(['icon']);
+    }
+
+    /**
+     * test error when creating an allergen without a name
+     * 
+     * @return void
+     */
+    public function testErrorWhenCreatingAnAllergenWithoutAName()
+    {
+        $data = [
+            'icon' => 'eicon eicon-circle-gluten',
+            'name' => null
+        ];
+
+        $response = $this->actingAs($this->user)->post(route('admin.allergens.store'), $data);
+
+        $this->assertDatabaseMissing(Allergen::getTableName(), $data);
+        $response->assertSessionHasErrors(['name']);
+    }
+
+    /**
+     * test can access the show page
+     * 
+     * @return void
+     */
+    public function testCanAccessAllergenShowPage()
+    {
+        $this->withoutExceptionHandling();
+        $allergen = Allergen::factory()->create();
+        
+        $response = $this->actingAs($this->user)->get(route('admin.allergens.show', [$allergen]));
+
+        $response->assertOk();
+        $response->assertViewIs('admin.allergens.show');
+    }
+
+    /**
+     * test denied access to the show page when not logged in
+     * 
+     * @return void
+     */
+    public function testDeniedAccessToAllergenShowPageWhenNotLoggedIn()
+    {
+        $allergen = Allergen::factory()->create();
+        
+        $response = $this->get(route('admin.allergens.show', [$allergen]));
+        
+        $this->assertGuest($guard = null);
+        $response->assertRedirect(route('login'));
+    }
+
+    /**
+     * test denied access to the show page without permission
+     * 
+     * @return void
+     */
+    public function testDeniedAccessToAllergenShowPageWithoutPermission()
+    {
+        $user = User::factory()->create();
+        $allergen = Allergen::factory()->create();
+        
+        $response = $this->actingAs($user)->get(route('admin.allergens.show', [$allergen]));
+
+        $this->assertAuthenticatedAs($user, $guard = null);
+        $response->assertForbidden();
+    }
+
+    /**
+     * test can access edit page
+     * 
+     * @return void
+     */
+    public function testCanAccessAllergenEditPage()
+    {
+        $this->withoutExceptionHandling();
+        $allergen = Allergen::factory()->create();
+        
+        $response = $this->actingAs($this->user)->get(route('admin.allergens.edit', [$allergen]));
+
+        $response->assertOk();
+        $response->assertViewIs('admin.allergens.edit');
+    }
+
+    /**
+     * test denied access to edit page when not logged in
+     * 
+     * @return void
+     */
+    public function testDeniedAccessToAllergenEditPageWhenNotLoggedIn()
+    {
+        $allergen = Allergen::factory()->create();
+        
+        $response = $this->get(route('admin.allergens.edit', [$allergen]));
+        
+        $this->assertGuest($guard = null);
+        $response->assertRedirect(route('login'));
+    }
+
+    /**
+     * test denied access to edit page without permission
+     * 
+     * @return void
+     */
+    public function testDeniedAccessToAllergenEditPageWithoutPermission()
+    {
+        $user = User::factory()->create();
+        $allergen = Allergen::factory()->create();
+        
+        $response = $this->actingAs($user)->get(route('admin.allergens.edit', [$allergen]));
+
+        $this->assertAuthenticatedAs($user, $guard = null);
+        $response->assertForbidden();
+    }
+
+    /**
+     * test a allergen can be updated
+     * 
+     * @return void
+     */
+    public function testCanEditAnAllergen()
+    {
+        $allergen = Allergen::factory()->create();
+        $data = [
+            'icon' => 'eicon eicon-circle-gluten',
+            'name' => $this->faker->word,
+        ];
+        
+        $response = $this->actingAs($this->user)->patch(route('admin.allergens.update', [$allergen]), $data);
+
+        $this->assertDatabaseHas(Allergen::getTableName(), $data);
+        $response->assertRedirect(route('admin.allergens.index'));
+    }
+
+    /**
+     * test denied access to update an allergen when not logged in
+     * 
+     * @return void
+     */
+    public function testDeniedAccessToUpdateAnAllergenWhenNotLoggedIn()
+    {
+        $allergen = Allergen::factory()->create();
+        $data = [
+            'icon' => 'eicon eicon-circle-gluten',
+            'name' => $this->faker->word,
+        ];
+        
+        $response = $this->patch(route('admin.allergens.update', [$allergen]), $data);
+        
+        $this->assertGuest($guard = null);
+        $response->assertRedirect(route('login'));
+    }
+
+    /**
+     * test denied access to update an allergen without permisison
+     * 
+     * @return void
+     */
+    public function testDeniedAccessToUpdateAnAllergenWithoutPermission()
+    {
+        $user = User::factory()->create();
+        $allergen = Allergen::factory()->create();
+        $data = [
+            'icon' => 'eicon eicon-circle-gluten',
+            'name' => $this->faker->word,
+        ];
+        
+        $response = $this->actingAs($user)->patch(route('admin.allergens.update', [$allergen]), $data);
+
+        $this->assertAuthenticatedAs($user, $guard = null);
+        $response->assertForbidden();
+    }
+
+    /**
+     * test session has an error when updating an allergen without a name
+     * 
+     * @return void
+     */
+    public function testErrorWhenUpdatingAnAllergenWithoutAName()
+    {
+        $allergen = Allergen::factory()->create();
+        $data = [
+            'icon' => 'eicon eicon-circle-gluten',
+            'name' => null
+        ];
+
+        $response = $this->actingAs($this->user)->patch(route('admin.allergens.update', [$allergen]), $data);
+
+        $this->assertDatabaseMissing(Allergen::getTableName(), $data);
+        $response->assertSessionHasErrors(['name']);
+    }
+
+    /**
+     * test session has an error when updating an allergen without an icon
+     * 
+     * @return void
+     */
+    public function testErrorWhenUpdatingAnAllergenWithoutAnIcon()
+    {
+        $allergen = Allergen::factory()->create();
+        $data = [
+            'icon' => null,
+            'name' => 'Gluten'
+        ];
+
+        $response = $this->actingAs($this->user)->patch(route('admin.allergens.update', [$allergen]), $data);
+
+        $this->assertDatabaseMissing(Allergen::getTableName(), $data);
+        $response->assertSessionHasErrors(['icon']);
+    }
+
+    /**
+     * test an allergen can be deleted
+     * 
+     * @return void
+     */
+    public function testCanDeleteAnAllergen()
+    {
+        $allergen = Allergen::factory()->create();
+        
+        $response = $this->actingAs($this->user)->delete(route('admin.allergens.destroy', [$allergen]));
+
+        $this->assertDatabaseMissing(Allergen::getTableName(), $allergen->toArray());
+        $response->assertRedirect(route('admin.allergens.index'));
+    }
+
+    /**
+     * test denied access to delete an allergen when not logged in
+     * 
+     * @return void
+     */
+    public function testDeniedDeletingAnAllergenWhenNotLoggedIn()
+    {
+        $allergen = Allergen::factory()->create();
+        
+        $response = $this->delete(route('admin.allergens.destroy', [$allergen]));
+        
+        $this->assertGuest($guard = null);
+        $response->assertRedirect(route('login'));
+    }
+
+    /**
+     * test denied access to delete an allergen without permission
+     * 
+     * @return void
+     */
+    public function testDeniedDeletingAnAllergenWithoutPermission()
+    {
+        $user = User::factory()->create();
+        $allergen = Allergen::factory()->create();
+        
+        $response = $this->actingAs($user)->delete(route('admin.allergens.destroy', [$allergen]));
+
+        $this->assertAuthenticatedAs($user, $guard = null);
+        $response->assertForbidden();
+    }
+}
