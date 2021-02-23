c156f074d0bc152a5ae8ec50721759f655266ff1
Justin Byrne <theskaterjesus@gmail.com>
2021-02-23 20:37:33 +0000

    Created Store and Update Requests

---
+++ app/Http/Requests/UpdateAllergenRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Gate;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateAllergenRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        abort_if(Gate::denies('allergen_edit'), 403);
+        
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'icon' => 'required',
+            'name' => 'required',
+        ];
+    }
+}
