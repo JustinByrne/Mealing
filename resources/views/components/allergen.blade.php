e740fbacfa483818550d646689df7e468ef38c53
Justin Byrne <theskaterjesus@gmail.com>
2021-02-27 11:57:28 +0000

    merged attributes for allergens

--- resources/views/components/allergen.blade.php
+++ resources/views/components/allergen.blade.php
@@ -6,17 +6,17 @@
 
 @switch($level)
     @case('may')
-        <span class="allergen-level-may">
+        <span {!! $attributes->merge(['class' => "allergen-level-may"]) !!}>
             <i class="{{ $icon }}" title="May Contain {{ $name }}"></i>
         </span>
         @break
     @case('yes')
-        <span class="allergen-level-yes">
+        <span {!! $attributes->merge(['class' => "allergen-level-yes"]) !!}>
             <i class="{{ $icon }}" title="Contains {{ $name }}"></i>
         </span>
         @break
     @default
-        <span class="allergen-level-no">
+        <span {!! $attributes->merge(['class' => "allergen-level-no"]) !!}>
             <i class="{{ $icon }}" title="Doesn't Contain {{ $name }}"></i>
         </span>
 @endswitch
