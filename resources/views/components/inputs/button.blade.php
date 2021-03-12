7453ae53e725385c4e22ab1343da5187fa0010fb
Justin Byrne <theskaterjesus@gmail.com>
2021-03-12 19:15:25 +0000

    Added ability to change colour of the button

--- resources/views/components/inputs/button.blade.php
+++ resources/views/components/inputs/button.blade.php
@@ -1,3 +1,5 @@
-<button {!! $attributes->merge(['class' => 'shadow bg-blue-500 text-white font-bold py-2 px-4 mb-2 rounded w-full md:w-auto hover:bg-blue-400']) !!}>
+@props(['color' => 'bg-blue-500 hover:bg-blue-400'])
+
+<button {!! $attributes->merge(['class' => 'shadow text-white font-bold py-2 px-4 mb-2 rounded w-full md:w-auto ' . $color]) !!}>
     {{ $slot }}
 </button>
