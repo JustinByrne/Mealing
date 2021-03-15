7d3c2560bee0b9a895b6239abbfe4eae3a948738
Justin Byrne <theskaterjesus@gmail.com>
2021-03-15 19:33:43 +0000

    changed password to no be required

--- app/Http/Requests/UpdateUserRequest.php
+++ app/Http/Requests/UpdateUserRequest.php
@@ -29,7 +29,7 @@ class UpdateUserRequest extends FormRequest
         return [
             'name' => 'required',
             'email' => 'required|unique:App\Models\User,email',
-            'password' => 'required|confirmed'
+            'password' => 'nullable|confirmed'
         ];
     }
 
@@ -44,7 +44,6 @@ class UpdateUserRequest extends FormRequest
             'name.required' => 'A name is required',
             'email.required' => 'An email is required',
             'email.unique' => 'This email has already been used',
-            'password.required' => 'A password is required',
             'password.confirmed' => 'The passwords do not match',
         ];
     }
