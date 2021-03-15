e86f4e539375622440c9d966a54dedcbfebe1c2f
Justin Byrne <theskaterjesus@gmail.com>
2021-03-15 19:13:43 +0000

    updated the admin sidebar

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -1,22 +1,44 @@
 @section('side-links')
-    <div>
-        <x-links.sidebar href="{{ route('admin.roles.index') }}" :active="request()->routeIs('admin.roles*')">
-            Roles
-        </x-links.sidebar>
+    @can ('user_access')
         <div>
-            <x-links.sidebar href="{{ route('admin.roles.create') }}" :active="request()->routeIs('admin.roles.create')">
-                <span class="font-bold mr-2">-</span> Create New Role
+            <x-links.sidebar href="{{ route('admin.users.index') }}" :active="request()->routeIs('admin.users*')">
+                Users
             </x-links.sidebar>
+            <div>
+                @can ('user_create')
+                    <x-links.sidebar href="{{ route('admin.users.create') }}" :active="request()->routeIs('admin.users.create')">
+                        <span class="font-bold mr-2">-</span> Create New User
+                    </x-links.sidebar>
+                @endcan
+            </div>
         </div>
-    </div>
-    <div>
-        <x-links.sidebar href="{{ route('admin.permissions.index') }}" :active="request()->routeIs('admin.permissions*')">
-            Permissions
-        </x-links.sidebar>
+    @endcan
+    @can ('role_access')
         <div>
-            <x-links.sidebar href="{{ route('admin.permissions.create') }}" :active="request()->routeIs('admin.permissions.create')">
-                <span class="font-bold mr-2">-</span> Create New Permission
+            <x-links.sidebar href="{{ route('admin.roles.index') }}" :active="request()->routeIs('admin.roles*')">
+                Roles
             </x-links.sidebar>
+            <div>
+                @can ('role_create')
+                    <x-links.sidebar href="{{ route('admin.roles.create') }}" :active="request()->routeIs('admin.roles.create')">
+                        <span class="font-bold mr-2">-</span> Create New Role
+                    </x-links.sidebar>
+                @endcan
+            </div>
         </div>
-    </div>
+    @endcan
+    @can ('permission_access')
+        <div>
+            <x-links.sidebar href="{{ route('admin.permissions.index') }}" :active="request()->routeIs('admin.permissions*')">
+                Permissions
+            </x-links.sidebar>
+            <div>
+                @can ('permission_create')
+                    <x-links.sidebar href="{{ route('admin.permissions.create') }}" :active="request()->routeIs('admin.permissions.create')">
+                        <span class="font-bold mr-2">-</span> Create New Permission
+                    </x-links.sidebar>
+                @endcan
+            </div>
+        </div>
+    @endcan
 @endsection
