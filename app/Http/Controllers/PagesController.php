a3ab5a94a29899d6b71f6727de332a4ece8aeca1
Justin Byrne <theskaterjesus@gmail.com>
2021-03-21 20:09:07 +0000

    Added design for dashboard

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Gate;
+use App\Models\Meal;
+use App\Models\User;
+use App\Models\Ingredient;
 use Illuminate\Http\Request;
 
 class PagesController extends Controller
@@ -35,7 +38,11 @@ class PagesController extends Controller
     public function adminDashboard()
     {
         abort_if(Gate::denies('admin_access'), 403);
+
+        $users = User::count();
+        $meals = Meal::count();
+        $ingredients = Ingredient::count();
         
-        return view('admin.dashboard');
+        return view('admin.dashboard', compact('users', 'meals', 'ingredients'));
     }
 }
