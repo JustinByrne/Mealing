e0c293153df9fb8a74ee1365212f83fc36657533
Justin Byrne <theskaterjesus@gmail.com>
2021-03-24 19:47:14 +0000

    fixed incorrect namespace and classes

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -62,6 +62,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'approved' => \Illuminate\Auth\Middleware\UserIsApproved::class,
+        'approved' => \App\Http\Middleware\UserIsApproved::class,
     ];
 }
--- app/Http/Middleware/UserIsApproved.php
+++ app/Http/Middleware/UserIsApproved.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use Auth;
 use Closure;
 use Illuminate\Http\Request;
 
