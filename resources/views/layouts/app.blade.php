dc553e6a3aad02bbf18e9135ee4e8d5029658565
Justin Byrne <theskaterjesus@gmail.com>
2021-04-24 16:00:35 +0100

    Updated the design of the layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,29 +1,64 @@
-<!DOCTYPE html>
+<!doctype html>
 <html>
     <head>
-        @include('layouts.head')
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link href="/css/app.css" rel="stylesheet">
+        <script src="https://kit.fontawesome.com/a357069ed8.js" crossorigin="anonymous"></script>
+        <script src="/js/app.js" defer></script>
+        <title>Layout</title>
     </head>
 
-    <body class="antialiased bg-blueGray-600">
-        @include('layouts.navigation')
-        <div class="container w-full flex flex-wrap mx-auto mt-6 lg:mt-12">
-            <div class="w-full ml-5 lg:ml-0 lg:w-1/5 lg:px-6 text-xl text-gray-800 leading-normal">    
-                <p class="text-base font-bold py-2 lg:pb-6 text-white">
-                    @if (trim($__env->yieldContent('side-links'))) Menu @endif
-                </p>
-                <div class="w-full sticky inset-0 overflow-x-hidden overflow-y-auto mt-0 z-20 lg:overflow-y-hidden lg:block">
-                    <ul class="list-reset">
-                        @yield('side-links')
-                    </ul>
+    <body>
+        <div class="w-full min-h-screen flex flex-col lg:flex-row bg-gray-100 dark:bg-gray-800">
+            <!-- Header -->
+            @include('layouts.header')
+            <div class="p-4 lg:p-6 w-full lg:ml-72">
+                <!-- Search Bar -->
+                <div class="flex flex-col lg:flex-row justify-between mb-4">
+                    <form action="" method="POST">
+                        <div class="relative">
+                            <button type="submit" class="absolute p-3 bg-green-600 bg-opacity-20 rounded-lg text-center text-green-600 dark:bg-opacity-30">
+                                <i class="fas fa-search"></i>
+                            </button>
+                            <input class="w-full max-w-xs pl-12 py-3 border-0 bg-green-600 bg-opacity-10 rounded-lg placeholder-gray-400 dark:bg-opacity-30 dark:text-gray-200" type="text" placeholder="Search..." name="s">
+                        </div>
+                    </form>
+                    <div class="hidden lg:block text-right relative" x-data="{ open: false }" @click.away="open = false">
+                        <div class="text-gray-600 flex items-center space-x-2 cursor-pointer dark:text-gray-200" @click="open = !open">
+                            <img class="rounded-full w-10 h-10 border-2 border-transparent hover:border-orange-300" src="{{ Auth::user()->getAvatar() }}" alt="{{ Auth::user()->name }}">
+                            <p>
+                                {{ Auth::user()->name }}
+                            </p>
+                            <button class="px-1.5">
+                                <i
+                                    class="fas"
+                                    :class="{ 'fa-angle-down': !open, 'fa-angle-up': open }"
+                                ></i>
+                            </button>
+                        </div>
+                        <div class="absolute right-0 z-10" :class="{ 'hidden': !open }">
+                            <div class="w-52 shadow-md rounded-md mt-1 py-2 bg-white text-left flex flex-col dark:bg-gray-700 dark:text-gray-200">
+                                <a href="{{ route('profile') }}" class="px-5 py-2 hover:bg-green-600 hover:text-white">
+                                    Profile
+                                </a>
+                                <a href="{{ route('profile.settings.account') }}" class="px-5 py-2 hover:bg-green-600 hover:text-white">
+                                    Settings
+                                </a>
+                                <form action="{{ route('logout') }}" method="POST">
+                                    <button type="submit" class="w-full text-left px-5 py-2 hover:bg-green-600 hover:text-white">
+                                        Logout
+                                    </button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Main content -->
+                <div class="w-full pb-3">
+
                 </div>
             </div>
-            @if (trim($__env->yieldContent('content')))
-                <div class="w-full p-8 my-6 text-gray-900 leading-normal shadow-lg bg-white rounded lg:mt-0 lg:w-4/5">
-                    @yield('content')
-                </div>
-            @endif
         </div>
-        @livewireScripts
-        @yield('scripts')
     </body>
 </html>
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,101 @@
+<header class="lg:fixed">
+    <div class="w-full bg-green-600 p-3 text-white lg:w-72 lg:h-screen" x-data="{open: false}">
+        <div class="flex justify-between items-center lg:mb-20 lg:mt-10">
+            <div class="flex text-lg font-bold lg:text-center lg:w-full lg:flex-col">
+                <i class="fas fa-pizza-slice pr-3 self-center lg:pr-0 lg:text-3xl lg:pb-3"></i>
+                Mealing
+            </div>
+            <div class="text-lg lg:hidden">
+                <button class="border border-gray-100 px-2 rounded-lg duration-300 ease-in-out transition text-center hover:bg-white hover:text-green-600" @click="open = !open">
+                    <i class="fas fa-bars"></i>
+                </button>
+            </div>
+        </div>
+        <div class="hidden lg:block mt-3" x-bind:class="{'hidden': !open}" @click.away="open = false">
+            <nav class="flex flex-col">
+                <a
+                    href="{{ route('homepage') }}"
+                    @if (request()->routeIs('homepage*'))
+                        class="w-full p-3 mb-3 font-bold text-green-600 bg-white rounded-lg space-x-2"
+                    @else
+                        class="w-full p-3 mb-3 font-bold rounded-lg hover:bg-white hover:text-green-600 space-x-2"
+                    @endif
+                >
+                    <i class="fas fa-home"></i>
+                    <span>
+                        Home
+                    </span>
+                </a>
+                <a
+                    href="/#"
+                    {{-- @if (request()->routeIs('homepage*'))
+                        class="w-full p-3 mb-3 font-bold text-green-600 bg-white rounded-lg space-x-2"
+                    @else --}}
+                        class="w-full p-3 mb-3 font-bold rounded-lg hover:bg-white hover:text-green-600 space-x-2"
+                    {{-- @endif --}}
+                >
+                    <i class="fas fa-utensils"></i>
+                    <span>
+                        Menus
+                    </span>
+                </a>
+                <a
+                    href="{{ route('meals.index') }}"
+                    @if (request()->routeIs('meals*'))
+                        class="w-full p-3 mb-3 font-bold text-green-600 bg-white rounded-lg space-x-2"
+                    @else
+                        class="w-full p-3 mb-3 font-bold rounded-lg hover:bg-white hover:text-green-600 space-x-2"
+                    @endif
+                >
+                    <i class="fas fa-hamburger"></i>
+                    <span>
+                        Meals
+                    </span>
+                </a>
+                @can ('admin_access')
+                    <a
+                        href="{{ route('admin.dashboard') }}"
+                        @if (request()->routeIs('admin*'))
+                            class="w-full p-3 mb-3 font-bold text-green-600 bg-white rounded-lg space-x-2"
+                        @else
+                            class="w-full p-3 mb-3 font-bold rounded-lg hover:bg-white hover:text-green-600 space-x-2"
+                        @endif
+                    >
+                        <i class="fas fa-tachometer-alt"></i>
+                        <span>
+                            Admin
+                        </span>
+                    </a>
+                @endcan
+            </nav>
+            <div class="w-full font-bold lg:hidden" x-data="{ open: false }" @click.away="open = false">
+                <div class="flex justify-between p-3 items-center" @click="open = !open">
+                    <p>
+                        {{ Auth::user()->name }}
+                    </p>
+                    <button>
+                        <i
+                            class="fas text-center cursor-pointer"
+                            :class="{ 'fa-angle-down': !open, 'fa-angle-up': open }"
+                        ></i>
+                    </button>
+                </div>
+                <div class="w-full font-bold text-green-600 bg-white rounded-lg space-x-2" :class="{ 'hidden': !open }">
+                    <div class="flex flex-col text-left">
+                        <a href="{{ route('profile') }}" class="px-3 py-2">
+                            Profile
+                        </a>
+                        <a href="{{ route('profile.settings.account') }}" class="px-3 py-2">
+                            Settings
+                        </a>
+                        <form action="{{ route('logout') }}" method="POST">
+                            <button type="submit" class="px-3 py-2 font-bold w-full text-left">
+                                Logout
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
