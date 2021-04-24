4b58ee1804a13d3eb0ba4dd37c11de0fa85ef697
Justin Byrne <theskaterjesus@gmail.com>
2021-04-24 16:36:09 +0100

    Created blank dashboard and user index

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,10 @@
+@extends('admin.layout')
+
+@section('admin.content')
+    <div class="grid grid-cols-1 lg:grid-cols-4 p-4">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+@endsection
---
+++ resources/views/admin/users/index.blade.php
@@ -0,0 +1,5 @@
+@extends('admin.layout')
+
+@section('admin.content')
+    
+@endsection
