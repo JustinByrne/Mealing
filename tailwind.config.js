bda99b8b3d4ad1c522269a92cecdc658cbfb72dc
Justin Byrne <theskaterjesus@gmail.com>
2021-04-24 15:28:54 +0100

    updated the tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,37 +1,15 @@
-const colors = require('tailwindcss/colors')
-
 module.exports = {
-  purge: [
-    './storage/framework/views/*.php',
-    './resources/views/**/*.blade.php',
-  ],
-  darkMode: false, // or 'media' or 'class'
+  purge: [],
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
-    extend: {
-      colors: {
-        blueGray: colors.blueGray,
-        coolGray: colors.coolGray,
-        gray: colors.gray,
-        trueGray: colors.trueGray,
-        warmGray: colors.warmGray,
-        orange: colors.orange,
-        amber: colors.amber,
-        yellow: colors.yellow,
-        lime: colors.lime,
-        green: colors.green,
-        emerald: colors.emerald,
-        teal: colors.teal,
-        cyan: colors.cyan,
-        lightBlue: colors.lightBlue,
-        violet: colors.violet,
-        purple: colors.purple,
-        fuchsia: colors.fuchsia,
-        rose: colors.rose,
-      },
-    },
-  },
-  variants: {
     extend: {},
   },
-  plugins: [],
+  variants: {
+    extend: {
+      borderWidth: ['hover']
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
 }
