f0a4fadb25cda723cdb7b627a9229cdc772262a4
Justin Byrne <theskaterjesus@gmail.com>
2021-04-27 11:07:04 +0100

    Added old values to create form

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -12,25 +12,25 @@
         <label for="name" class="dark:text-gray-200 self-center">
             Name
         </label>
-        <input type="text" name="name" id="name" placeholder="" class="border-1 border-gray-100 shadow bg-opacity-20 rounded-lg placeholder-gray-500 w-full lg:w-60 focus:outline-none focus:ring-1 focus:border-green-500 focus:ring-green-500 dark:bg-gray-900 dark:border-transparent dark:text-gray-200">
+        <input type="text" name="name" id="name" value="{{ old('name') }}" class="border-1 border-gray-100 shadow bg-opacity-20 rounded-lg placeholder-gray-500 w-full lg:w-60 focus:outline-none focus:ring-1 focus:border-green-500 focus:ring-green-500 dark:bg-gray-900 dark:border-transparent dark:text-gray-200">
     </div>
     <div class="grid grid-cols-1 lg:grid-cols-6">
         <label for="email" class="dark:text-gray-200 self-center">
             Email
         </label>
-        <input type="email" name="email" id="email" placeholder="" class="border-1 border-gray-100 shadow bg-opacity-20 rounded-lg placeholder-gray-500 w-full lg:w-60 focus:outline-none focus:ring-1 focus:border-green-500 focus:ring-green-500 dark:bg-gray-900 dark:border-transparent dark:text-gray-200">
+        <input type="email" name="email" id="email" value="{{ old('email') }}" class="border-1 border-gray-100 shadow bg-opacity-20 rounded-lg placeholder-gray-500 w-full lg:w-60 focus:outline-none focus:ring-1 focus:border-green-500 focus:ring-green-500 dark:bg-gray-900 dark:border-transparent dark:text-gray-200">
     </div>
     <div class="grid grid-cols-1 lg:grid-cols-6">
         <label for="password" class="dark:text-gray-200 self-center">
             Password
         </label>
-        <input type="password" name="password" id="password" placeholder="" class="border-1 border-gray-100 shadow bg-opacity-20 rounded-lg placeholder-gray-500 w-full lg:w-60 focus:outline-none focus:ring-1 focus:border-green-500 focus:ring-green-500 dark:bg-gray-900 dark:border-transparent dark:text-gray-200">
+        <input type="password" name="password" id="password" class="border-1 border-gray-100 shadow bg-opacity-20 rounded-lg placeholder-gray-500 w-full lg:w-60 focus:outline-none focus:ring-1 focus:border-green-500 focus:ring-green-500 dark:bg-gray-900 dark:border-transparent dark:text-gray-200">
     </div>
     <div class="grid grid-cols-1 lg:grid-cols-6">
         <label for="password_confirmation" class="dark:text-gray-200 self-center">
             Confirm Password
         </label>
-        <input type="password" name="password_confirmation" id="password_confirmation" placeholder="" class="border-1 border-gray-100 shadow bg-opacity-20 rounded-lg placeholder-gray-500 w-full lg:w-60 focus:outline-none focus:ring-1 focus:border-green-500 focus:ring-green-500 dark:bg-gray-900 dark:border-transparent dark:text-gray-200">
+        <input type="password" name="password_confirmation" id="password_confirmation" class="border-1 border-gray-100 shadow bg-opacity-20 rounded-lg placeholder-gray-500 w-full lg:w-60 focus:outline-none focus:ring-1 focus:border-green-500 focus:ring-green-500 dark:bg-gray-900 dark:border-transparent dark:text-gray-200">
     </div>
     <div class="">
         <button type="submit" class="w-full lg:w-auto rounded shadow-md py-2 px-4 bg-green-600 text-white hover:bg-green-500">
