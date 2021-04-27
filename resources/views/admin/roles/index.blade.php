49fea2fd8ea863a33c22e61fa46b9991b14d8953
Justin Byrne <theskaterjesus@gmail.com>
2021-04-27 11:13:53 +0100

    Added roles index

---
+++ resources/views/admin/roles/index.blade.php
@@ -0,0 +1,5 @@
+@extends('admin.layout')
+
+@section('admin.content')
+    @livewire('roles.index')
+@endsection
---
+++ resources/views/livewire/roles/index.blade.php
@@ -0,0 +1,41 @@
+<div class="w-full p-4 bg-white border-b border-gray-200 rounded-t-md dark:bg-gray-700">
+    <p class="font-bold dark:text-gray-200">
+        All Roles
+    </p>
+</div>
+<div class="p-4">
+    <table class="w-full dark:text-gray-200">
+        <thead>
+            <tr class="hidden lg:table-row border-b">
+                <th class="text-left px-4 py-2 w-1/3">
+                    Name
+                </th>
+                <th class="text-left px-4 py-2 w-1/3">
+                    # of Users
+                </th>
+                <th class="text-left px-4 py-2 w-1/3">
+                    # Permissions
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($roles as $role)
+                @if ($loop->odd)
+                    <tr class="hover:bg-green-600 hover:bg-opacity-10">
+                @else
+                    <tr class="bg-green-600 bg-opacity-5 hover:bg-opacity-10">
+                @endif
+                    <td class="block lg:table-cell px-4 py-2">
+                        {{ $role->name }}
+                    </td>
+                    <td class="block lg:table-cell px-4 py-2">
+                        {{ $role->users->count()}}
+                    </td>
+                    <td class="block lg:table-cell px-4 py-2">
+                        {{ $role->permissions->count()}}
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
