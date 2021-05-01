41007230cd1a84f4f1e01a87e64f3303b9379e9c
Justin Byrne <theskaterjesus@gmail.com>
2021-05-01 15:32:57 +0100

    Added user profile page

---
+++ resources/views/user/profile/index.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="bg-white rounded-md dark:bg-gray-700">
+    <div class="w-full p-4 border-b border-gray-200 ">
+        <p class="font-bold dark:text-gray-200">
+            User Profile
+        </p>
+    </div>
+    <div class="p-3">
+        <div class="grid grid-cols-1 lg:grid-cols-4 gap-6 w-full">
+            <div class="col-span-full mb-4 lg:mb-0 lg:col-span-1">
+                <img class="rounded-full mx-auto shadow-lg" src="{{ $user->getAvatar(300) }}" alt="{{ $user->getFullName() }}">
+            </div>
+            <div class="col-span-full lg:col-span-3">
+                <div class="text-sm font-light leading-relaxed pt-10 mb-2 text-lightGray-800">
+                    <p class="text-sm mb-5 dark:text-gray-200">
+                        <strong>Name:</strong> {{ $user->getFullName() }}<br>
+                        <strong>Email:</strong> {{ $user->email }}<br>
+                        <strong>Subscripiton:</strong> Free
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
