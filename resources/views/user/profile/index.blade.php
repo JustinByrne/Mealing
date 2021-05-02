a94cac677c5654d67f99d9891b03e5d639c7c7e4
Justin Byrne <theskaterjesus@gmail.com>
2021-05-02 20:56:06 +0100

    Changed to Auth::user()

--- resources/views/user/profile/index.blade.php
+++ resources/views/user/profile/index.blade.php
@@ -7,16 +7,16 @@
             User Profile
         </p>
     </div>
-    <div class="p-3">
+    <div class="p-4">
         <div class="grid grid-cols-1 lg:grid-cols-4 gap-6 w-full">
             <div class="col-span-full mb-4 lg:mb-0 lg:col-span-1">
-                <img class="rounded-full mx-auto shadow-lg" src="{{ $user->getAvatar(300) }}" alt="{{ $user->getFullName() }}">
+                <img class="rounded-full mx-auto shadow-lg" src="{{ Auth::user()->getAvatar(300) }}" alt="{{ Auth::user()->getFullName() }}">
             </div>
             <div class="col-span-full lg:col-span-3">
                 <div class="text-sm font-light leading-relaxed pt-10 mb-2 text-lightGray-800">
                     <p class="text-sm mb-5 dark:text-gray-200">
-                        <strong>Name:</strong> {{ $user->getFullName() }}<br>
-                        <strong>Email:</strong> {{ $user->email }}<br>
+                        <strong>Name:</strong> {{ Auth::user()->getFullName() }}<br>
+                        <strong>Email:</strong> {{ Auth::user()->email }}<br>
                         <strong>Subscripiton:</strong> Free
                     </p>
                 </div>
