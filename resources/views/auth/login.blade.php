d545546476fc9623e5e124758b7d38f83ff14aa5
Justin Byrne <theskaterjesus@gmail.com>
2021-05-08 13:46:41 +0100

    updated the design to make it match

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -28,21 +28,20 @@
         <input type="password" placeholder="Password" id="password" name="password" class="border-1 border-gray-100 shadow bg-opacity-20 rounded-lg placeholder-gray-500 w-full lg:w-60 focus:outline-none focus:ring-1 focus:border-green-500 focus:ring-green-500 dark:bg-gray-900 dark:border-transparent dark:text-gray-200">
     </div>
     <div class="grid grid-cols-1 lg:grid-cols-3 items-center">
+        <div class="lg:col-start-2 col-span-2">
+            <a href="{{ route('password.request') }}" class="text-sm hover:text-yellow-600 dark:text-gray-200 dark:hover:text-yellow-600">
+                Forgotten My Password
+            </a>
+        </div>
+    </div>
+    <div class="grid grid-cols-1 lg:grid-cols-3 items-center">
         <div class="lg:col-start-2">
-            <p class="mb-5 dark:text-gray-200 space-x-2">
+            <p class="dark:text-gray-200 space-x-2">
                 <input type="checkbox" name="remember" class="rounded text-green-600 focus:outline-none focus:ring-1 focus:border-green-500 focus:ring-green-500 dark:bg-gray-900 dark:border-transparent dark:text-gray-200 dark:focus:bg-green-600">
                 <label for="remember">
                     Remember Me
                 </label>
             </p>
-        </div>
-    </div>
-    <div class="grid grid-cols-1 md:grid-cols-4">
-        <div></div>
-        <div class="md:col-span-3">
-            <a href="{{ route('password.request') }}" class="hover:text-yellow-600 dark:text-gray-200 dark:hover:text-yellow-600">
-                Forgotten My Password
-            </a>
         </div>
     </div>
     <div class="grid grid-cols-1 lg:grid-cols-2 gap-2 lg:gap-3 items-center">
