63e1428cabb46511f4aceaab4f50bc190960ccc5
Justin Byrne <theskaterjesus@gmail.com>
2021-05-11 14:56:02 +0100

    Added number of meals

--- resources/views/livewire/ingredients/index.blade.php
+++ resources/views/livewire/ingredients/index.blade.php
@@ -11,7 +11,10 @@
                     <th class="text-left px-4 py-2 w-1/3">
                         Name
                     </th>
-                    <th class="text-left px-4 py-2 w-2/3"></th>
+                    <th class="text-left px-4 py-2 w-1/3">
+                        Number of Meals
+                    </th>
+                    <th class="text-left px-4 py-2 w-1/3"></th>
                 </tr>
             </thead>
             <tbody>
@@ -25,6 +28,9 @@
                             <a href="{{ route('admin.ingredients.show', $ingredient) }}" class="hover:text-yellow-600 dark:text-gray-200 dark:hover:text-yellow-600">
                                 {{ $ingredient->name }}
                             </a>
+                        </td>
+                        <td class="block lg:table-cell px-4 py-2">
+                            {{ $ingredient->meals->count() }}
                         </td>
                         <td class="flex flex-col lg:flex-row px-4 py-2 lg:justify-end space-y-1 lg:space-y-0 lg:space-x-1">
                             <div>
