f5bbe5de934688a91e721a6a646e25da10e52a1d
Justin Byrne <theskaterjesus@gmail.com>
2021-05-11 14:55:11 +0100

    Added edit and delete buttons

--- resources/views/livewire/ingredients/index.blade.php
+++ resources/views/livewire/ingredients/index.blade.php
@@ -11,6 +11,7 @@
                     <th class="text-left px-4 py-2 w-1/3">
                         Name
                     </th>
+                    <th class="text-left px-4 py-2 w-2/3"></th>
                 </tr>
             </thead>
             <tbody>
@@ -21,9 +22,25 @@
                         <tr class="bg-green-600 bg-opacity-5 hover:bg-opacity-10">
                     @endif
                         <td class="block lg:table-cell px-4 py-2">
-                            <a href="{{ route('admin.ingredients.edit', $ingredient) }}" class="hover:text-yellow-600 dark:text-gray-200 dark:hover:text-yellow-600">
+                            <a href="{{ route('admin.ingredients.show', $ingredient) }}" class="hover:text-yellow-600 dark:text-gray-200 dark:hover:text-yellow-600">
                                 {{ $ingredient->name }}
                             </a>
+                        </td>
+                        <td class="flex flex-col lg:flex-row px-4 py-2 lg:justify-end space-y-1 lg:space-y-0 lg:space-x-1">
+                            <div>
+                                <a href="{{ route('admin.ingredients.edit', $ingredient) }}">
+                                    <button class="w-full lg:w-auto rounded shadow-md py-1 px-2 bg-green-600 text-white hover:bg-green-500 text-xs">
+                                        <i class="fas fa-pencil-alt"></i>
+                                    </button>
+                                </a>
+                            </div>
+                            <div>
+                                <form action="{{ route('admin.ingredients.destroy', $ingredient) }}" method="POST">
+                                    <button class="w-full lg:w-auto rounded shadow-md py-1 px-2 bg-gray-400 text-white hover:bg-gray-300 text-xs">
+                                        <i class="fas fa-trash"></i>
+                                    </button>
+                                </form>
+                            </div>
                         </td>
                     </tr>
                 @endforeach
