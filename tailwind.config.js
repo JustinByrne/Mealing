fbcabf4646e085f2655b887f07f928ea8d3ad3c5
Justin Byrne <theskaterjesus@gmail.com>
2021-05-15 16:50:13 +0100

    Added new screen height options

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@ module.exports = {
   purge: [],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      height: theme => ({
+        "screen/2": "50vh",
+        "screen/3": "calc(100vh / 3)",
+        "screen/4": "calc(100vh / 4)",
+        "screen/5": "calc(100vh / 5)",
+      })
+    },
   },
   variants: {
     extend: {
