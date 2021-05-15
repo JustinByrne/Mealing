0e03e709bd088b2fb8259261ad77ff4007d542e1
Justin Byrne <theskaterjesus@gmail.com>
2021-05-15 13:28:33 +0100

    Added rating to query

--- app/Http/Livewire/Meals/Index.php
+++ app/Http/Livewire/Meals/Index.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Livewire\Meals;
 
-use Livewire\Component;
 use App\Models\Meal;
+use Livewire\Component;
+use Illuminate\Support\Facades\DB;
 
 class Index extends Component
 {
@@ -12,7 +13,9 @@ class Index extends Component
     
     public function render()
     {
-        $meals = Meal::with('ratings', 'media')->filter()->paginate(15);
+        $meals = Meal::with('ratings', 'media')->withCount(['ratings as rating' => function($query) {
+            $query->select(DB::raw('coalesce(avg(score),0)'));
+        }])->filter()->paginate(15);
         
         return view('livewire.meals.index', compact('meals'));
     }
