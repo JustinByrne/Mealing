a0d66bca997ce0978c903cc997ec8ef22236a198
Justin Byrne <theskaterjesus@gmail.com>
2021-05-16 22:03:38 +0100

    Added create meal page

---
+++ resources/views/livewire/meals/create.blade.php
@@ -0,0 +1,77 @@
+<div>
+    <div class="items-top space-y-4 md:space-y-0 md:grid md:grid-cols-9 md:space-x-6">
+        <div>
+            <input type="text" placeholder="Amount" wire:model="quantity" class="border-1 border-gray-100 shadow bg-opacity-20 rounded-lg placeholder-gray-500 w-full focus:outline-none focus:ring-1 focus:border-green-500 focus:ring-green-500 dark:bg-gray-900 dark:border-transparent dark:text-gray-200">
+        </div>
+        <div class="w-full relative md:col-span-4">
+            <input type="text" placeholder="Search Ingredients..." wire:model.debounce.300ms="query" wire:keydown.escape="resetQuery" wire:keydown.tab="resetQuery" class="border-1 border-gray-100 shadow bg-opacity-20 rounded-lg placeholder-gray-500 w-full focus:outline-none focus:ring-1 focus:border-green-500 focus:ring-green-500 dark:bg-gray-900 dark:border-transparent dark:text-gray-200">
+            
+            <div class="absolute z-10 w-full bg-white dark:bg-gray-700 rounded-b-lg shadow-lg">
+                <p wire:loading wire:target="query" class="px-2 py-1 dark:text-gray-200">
+                    Searching...
+                </p>
+            </div>
+
+            @if (!empty($query) && !$autocomplete)
+                <div class="absolute z-10 w-full bg-white dark:bg-gray-700  rounded-b-lg shadow-lg">
+                    @if (!empty($ingredients))
+                        @foreach ($ingredients as $ingredient)
+                            <p
+                                @if ($loop->last)
+                                    class="px-2 py-1 cursor-pointer rounded-b-lg hover:bg-blueGray-200 dark:text-gray-200"
+                                @else
+                                    class="px-2 py-1 cursor-pointer hover:bg-blueGray-200 dark:text-gray-200"
+                                @endif
+                                wire:click="autocomplete('{{ $ingredient['name'] }}', {{ $ingredient['id'] }})"
+                            >
+                                {{ $ingredient['name'] }}
+                            </p>
+                        @endforeach
+                        @foreach ($ingredients as $ingredient)
+                            @if (in_array(ucwords($query), $ingredient))
+                                @break
+                            @endif
+
+                            @if ($loop->last)
+                                <p class="px-2 py-1 cursor-pointer rounded-b-lg dark:text-gray-200" wire:click.prevent="createIngredient()">
+                                    Add "{{ $query }}"
+                                </p>
+                        @endif
+                        @endforeach
+                    @else
+                        @can ('ingredient_create')
+                            <p class="px-2 py-1 cursor-pointer rounded-b-lg dark:text-gray-200" wire:click.prevent="createIngredient()">
+                                No Results - Add "{{ $query }}"
+                            </p>
+                        @else
+                            <p class="px-2 py-1 cursor-pointer rounded-b-lg dark:text-gray-200">
+                                No Results
+                            </p>
+                        @endcan
+                    @endif
+                </div>
+            @endif
+        </div>
+        <div class="md:pl-6">
+            <button wire:click.prevent="add({{ $i }})" class="w-full lg:w-auto rounded shadow-md py-2 px-4 bg-green-600 text-white hover:bg-green-500">
+                <i class="fas fa-plus"></i>
+            </button>
+        </div>
+    </div>
+    <div>
+        @if (!empty($inputs))
+            <ul>
+                @foreach ($inputs as $key => $item)
+                    <li class="font-light text-xs md:pt-2 md:text-base dark:text-gray-200">
+                        {{ $item['quantity'] }} - {{ $item['ingredient'] }}
+                        <button class="w-full lg:w-auto rounded shadow-md py-1 px-2 bg-gray-400 text-white hover:bg-gray-300 text-xs">
+                            <i class="fas fa-minus"></i>
+                        </button>
+                        <input type="hidden" name="quantities[{{ $key }}]" value="{{ $item['quantity'] }}">
+                        <input type="hidden" name="ingredients[{{ $key }}]" value="{{ $item['ingredientId'] }}">
+                    </li>
+                @endforeach
+            </ul>
+        @endif
+    </div>
+</div>
---
+++ resources/views/meals/create.blade.php
@@ -0,0 +1,139 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="w-full rounded-md shadow-md bg-white dark:bg-gray-700">
+    <div class="w-full p-4 border-b border-gray-200">
+        <p class="font-bold dark:text-gray-200">
+            Create New Meal
+        </p>
+    </div>
+    <div class="p-3">
+        <form action="{{ route('meals.store') }}" method="POST" enctype="multipart/form-data">
+            <div class="space-y-4">
+                <div class="items-start md:grid md:grid-cols-9 md:space-x-6">
+                    <label for="name" class="dark:text-gray-200 self-center">
+                        Name
+                    </label>
+                    <div class="w-full md:col-span-4">
+                        <input type="text" name="name" id="name" value="{{ Request::old('name') }}" class="border-1 border-gray-100 shadow bg-opacity-20 rounded-lg placeholder-gray-500 w-full lg:w-60 focus:outline-none focus:ring-1 focus:border-green-500 focus:ring-green-500 dark:bg-gray-900 dark:border-transparent dark:text-gray-200">
+                        @error('name')
+                            <p class="text-red-500 italic text-xs font-light">
+                                {{ $message }}
+                            </p>
+                        @enderror
+                    </div>
+                </div>
+                <div class="items-start md:grid md:grid-cols-9 md:space-x-6">
+                    <label for="servings" class="dark:text-gray-200 self-center">
+                        # of Servings
+                    </label>
+                    <div class="w-full md:col-span-4">
+                        <input type="text" name="servings" id="servings" value="{{ Request::old('servings') }}" class="border-1 border-gray-100 shadow bg-opacity-20 rounded-lg placeholder-gray-500 w-full lg:w-60 focus:outline-none focus:ring-1 focus:border-green-500 focus:ring-green-500 dark:bg-gray-900 dark:border-transparent dark:text-gray-200">
+                        @error('servings')
+                            <p class="text-red-500 italic text-xs font-light">
+                                {{ $message }}
+                            </p>
+                        @enderror
+                    </div>
+                </div>
+                <div class="items-start md:grid md:grid-cols-9 md:space-x-6">
+                    <label for="timing" class="dark:text-gray-200 self-center">
+                        Time in Minutes
+                    </label>
+                    <div class="w-full md:col-span-4">
+                        <input type="text" name="timing" id="timing" value="{{ Request::old('timing') }}" class="border-1 border-gray-100 shadow bg-opacity-20 rounded-lg placeholder-gray-500 w-full lg:w-60 focus:outline-none focus:ring-1 focus:border-green-500 focus:ring-green-500 dark:bg-gray-900 dark:border-transparent dark:text-gray-200">
+                        @error('timing')
+                            <p class="text-red-500 italic text-xs font-light">
+                                {{ $message }}
+                            </p>
+                        @enderror
+                    </div>
+                </div>
+                <div class="items-start md:grid md:grid-cols-9 md:space-x-6">
+                    <label for="image" class="dark:text-gray-200 self-center">
+                        Image
+                    </label>
+                    <div class="w-full md:col-span-4">
+                        <input type="file" name="image" class="dark:text-gray-200 self-center">
+                    </div>
+                </div>
+                <div class="space-y-2 md:flex md:space-y-0 md:space-x-3">
+                    <div>
+                        <input type="checkbox" name="adult" id="adult" value="1" class="rounded text-green-600 focus:outline-none focus:ring-1 focus:border-green-500 focus:ring-green-500 dark:bg-gray-900 dark:border-transparent dark:text-gray-200 dark:focus:bg-green-600">
+                        <label for="adult" class="dark:text-gray-200 self-center">
+                            Suitable for Adults
+                        </label>
+                    </div>
+                    <div>
+                        <input type="checkbox" name="kids" id="kids" value="1" class="rounded text-green-600 focus:outline-none focus:ring-1 focus:border-green-500 focus:ring-green-500 dark:bg-gray-900 dark:border-transparent dark:text-gray-200 dark:focus:bg-green-600">
+                        <label for="kids" class="dark:text-gray-200 self-center">
+                            Suitable for Children
+                        </label>
+                    </div>
+                </div>
+                <div class="items-start md:grid md:grid-cols-9 md:space-x-6">
+                    <label class="dark:text-gray-200 self-center">
+                        Allergens
+                    </label>
+                    <div class="w-full md:col-span-4 md:pt-1 text-2xl">
+                        @foreach ($allergens as $allergen)
+                            <div class="inline cursor-pointer" x-data="{ level: 'no' }">
+                                <span
+                                    x-on:click="level = changeLevel(level)"
+                                    x-bind:class="{
+                                        'allergen-level-no': level == 'no',
+                                        'allergen-level-may': level == 'may',
+                                        'allergen-level-yes': level == 'yes',
+                                    }"
+                                >
+                                    <i class="{{ $allergen->icon }}" title="{{ $allergen->name }}"></i>
+                                </span>
+                                <input type="hidden" name="allergens[{{ $allergen->id }}]" x-model="level">
+                            </div>
+                        @endforeach
+                    </div>
+                </div>
+                <div class="md:space-y-2">
+                    <label class="dark:text-gray-200 self-center">
+                        Ingredients
+                    </label>
+                    @error('ingredients')
+                            <p class="text-red-500 italic text-xs font-light">
+                                {{ $message }}
+                            </p>
+                        @enderror
+                    @livewire('meals.create')
+                </div>
+                <div class="md:space-y-2">
+                    <label for="instruction" class="dark:text-gray-200 self-center">
+                        Instructions
+                    </label>
+                    <div>
+                        <textarea name="instruction" id="instruction" class="border-1 border-gray-100 shadow bg-opacity-20 rounded-lg placeholder-gray-500 w-full h-64 focus:outline-none focus:ring-1 focus:border-green-500 focus:ring-green-500 dark:bg-gray-900 dark:border-transparent dark:text-gray-200"></textarea>
+                    </div>
+                </div>
+                <div>
+                    <button type="submit" class="w-full lg:w-auto rounded shadow-md py-2 px-4 bg-green-600 text-white hover:bg-green-500">
+                        Add Meal
+                    </button>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
+<script type="text/javascript">
+    function changeLevel($current)  {
+        switch($current) {
+            case 'no':
+                return 'may';
+                break;
+            case 'may':
+                return 'yes';
+                break;
+            case 'yes':
+                return 'no';
+                break;
+        }
+    }
+</script>
+@endsection
