7363a421b776278350f1285b11a89d0de960ffa2
Justin Byrne <theskaterjesus@gmail.com>
2021-05-20 19:04:44 +0100

    Changed link from text to whole card

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -6,33 +6,33 @@
 </p>
 <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-6 mb-5">
     @foreach ($topMeals as $meal)
-        <div class="w-full rounded-md shadow-md dark:bg-gray-700">
-            <div>
-                @if( $meal->getMedia()->count() > 0)
-                    <img src="{{ $meal->getFirstMediaUrl() }}" class="w-full max-h-32 object-cover rounded-t-md">
-                @else
-                    <img src="https://via.placeholder.com/640x360.png?text=No+Image" class="w-full max-h-32 object-cover rounded-t-md">
-                @endif
-            </div>
-            <div class="bg-white p-4 rounded-b-md dark:bg-gray-700 dark:text-gray-200">
-                <p class="mb-2">
-                    <a href="{{ route('meals.show', $meal) }}" class="hover:text-yellow-600 dark:text-gray-200 dark:hover:text-yellow-600">
+        <a href="{{ route('meals.show', $meal) }}">
+            <div class="w-full rounded-md shadow-md dark:bg-gray-700">
+                <div>
+                    @if( $meal->getMedia()->count() > 0)
+                        <img src="{{ $meal->getFirstMediaUrl() }}" class="w-full max-h-32 object-cover rounded-t-md">
+                    @else
+                        <img src="https://via.placeholder.com/640x360.png?text=No+Image" class="w-full h-32 object-cover rounded-t-md">
+                    @endif
+                </div>
+                <div class="bg-white p-4 rounded-b-md dark:bg-gray-700 dark:text-gray-200">
+                    <p class="mb-2">
                         {{ $meal->name }}
-                    </a>
-                </p>
-                <p class="text-sm text-yellow-400" title="{{ $meal->avg_rating > 0 ? $meal->avg_rating : '0' }}">
-                    @for ($x = 0; $x < 5; $x++)
-                        @if (floor($meal->avg_rating)-$x >= 1)
-                            <i class="fas fa-star"></i>
-                        @elseif ($meal->avg_rating-$x > 0)
-                            <i class="fas fa-star-half-alt"></i>
-                        @else
-                            <i class="far fa-star"></i>
-                        @endif
-                    @endfor
-                </p>
+                    </p>
+                    <p class="text-sm text-yellow-400" title="{{ $meal->avg_rating > 0 ? $meal->avg_rating : '0' }}">
+                        @for ($x = 0; $x < 5; $x++)
+                            @if (floor($meal->avg_rating)-$x >= 1)
+                                <i class="fas fa-star"></i>
+                            @elseif ($meal->avg_rating-$x > 0)
+                                <i class="fas fa-star-half-alt"></i>
+                            @else
+                                <i class="far fa-star"></i>
+                            @endif
+                        @endfor
+                    </p>
+                </div>
             </div>
-        </div>
+        </a>
     @endforeach
 </div>
 @endsection
--- resources/views/livewire/meals/index.blade.php
+++ resources/views/livewire/meals/index.blade.php
@@ -2,36 +2,36 @@
     <div class="w-full lg:w-4/5 order-2 lg:order-1">
         <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6 mb-5">
             @foreach ($meals as $meal)
-                <div class="w-full rounded-md shadow-md dark:bg-gray-700">
-                    <div>
-                        @if( $meal->getMedia()->count() > 0)
-                            <img src="{{ $meal->getFirstMediaUrl() }}" class="w-full max-h-32 object-cover rounded-t-md">
-                        @else
-                            <img src="https://via.placeholder.com/640x360.png?text=No+Image" class="w-full max-h-32 object-cover rounded-t-md">
-                        @endif
-                    </div>
-                    <div class="bg-white p-4 rounded-b-md dark:bg-gray-700 dark:text-gray-200 space-y-2">
-                        <p>
-                            <a href="{{ route('meals.show', $meal) }}" class="hover:text-yellow-600 dark:text-gray-200 dark:hover:text-yellow-600">
+                <a href="{{ route('meals.show', $meal) }}">
+                    <div class="w-full rounded-md shadow-md dark:bg-gray-700">
+                        <div>
+                            @if( $meal->getMedia()->count() > 0)
+                                <img src="{{ $meal->getFirstMediaUrl() }}" class="w-full max-h-32 object-cover rounded-t-md">
+                            @else
+                                <img src="https://via.placeholder.com/640x360.png?text=No+Image" class="w-full max-h-32 object-cover rounded-t-md">
+                            @endif
+                        </div>
+                        <div class="bg-white p-4 rounded-b-md dark:bg-gray-700 dark:text-gray-200 space-y-2">
+                            <p>
                                 {{ $meal->name }}
-                            </a>
-                        </p>
-                        <p class="text-sm text-yellow-400" title="{{ $meal->avg_rating > 0 ? $meal->avg_rating : '0' }}">
-                            @for ($x = 0; $x < 5; $x++)
-                                @if (floor($meal->avg_rating)-$x >= 1)
-                                    <i class="fas fa-star"></i>
-                                @elseif ($meal->avg_rating-$x > 0)
-                                    <i class="fas fa-star-half-alt"></i>
-                                @else
-                                    <i class="far fa-star"></i>
-                                @endif
-                            @endfor
-                        </p>
-                        <p>
-                            Serves: {{ $meal->servings }}
-                        </p>
+                            </p>
+                            <p class="text-sm text-yellow-400" title="{{ $meal->avg_rating > 0 ? $meal->avg_rating : '0' }}">
+                                @for ($x = 0; $x < 5; $x++)
+                                    @if (floor($meal->avg_rating)-$x >= 1)
+                                        <i class="fas fa-star"></i>
+                                    @elseif ($meal->avg_rating-$x > 0)
+                                        <i class="fas fa-star-half-alt"></i>
+                                    @else
+                                        <i class="far fa-star"></i>
+                                    @endif
+                                @endfor
+                            </p>
+                            <p>
+                                Serves: {{ $meal->servings }}
+                            </p>
+                        </div>
                     </div>
-                </div>
+                </a>
             @endforeach
         </div>
         {{ $meals->links() }}
