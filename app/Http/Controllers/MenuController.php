c1de129d214e5452a078d9a10e3315f503b1416c
Justin Byrne <theskaterjesus@gmail.com>
2021-05-22 22:17:43 +0100

    Added create and store functions

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Gate;
 use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
 
 class MenuController extends Controller
 {
@@ -12,5 +13,17 @@ class MenuController extends Controller
         abort_if(Gate::denies('menu_access'), 403);
 
         //
+    }
+
+    public function create(): void
+    {
+        abort_if(Gate::denies('menu_create'), 403);
+    }
+
+    public function store(Request $request): RedirectResponse
+    {
+        abort_if(Gate::denies('menu_create'), 403);
+
+        return redirect()->route('menu.index');
     }
 }
