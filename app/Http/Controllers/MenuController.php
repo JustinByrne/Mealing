7ebaa0c95a041ef153b86c0d2a7585ad75d9e32e
Justin Byrne <theskaterjesus@gmail.com>
2021-05-25 20:41:39 +0100

    Added create function for menu

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use Gate;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\RedirectResponse;
 
 class MenuController extends Controller
@@ -23,6 +25,26 @@ class MenuController extends Controller
     public function store(Request $request): RedirectResponse
     {
         abort_if(Gate::denies('menu_create'), 403);
+
+        $menu = Auth::user()->menus()->create();
+
+        $wc = Carbon::parse($request->input('wc', Carbon::now()->startOfWeek()->format('Y-m-d H:i')));
+
+        $days = [
+            'monday' => 0,
+            'tuesday' => 1,
+            'wednesday' =>2,
+            'thursday' =>3,
+            'friday' => 4,
+            'saturday' => 5,
+            'sunday' => 6
+        ];
+
+        foreach ($days as $day => $add) {
+            if (! is_null($request->input($day))) {
+                $menu->meals()->attach($request->input($day), ['date' => $wc->addDays($add)]);
+            }
+        }
 
         return redirect()->route('menus.index');
     }
