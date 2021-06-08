bc9f754d37bd34b0d47177f9e4b46bc7af729e91
Justin Byrne <theskaterjesus@gmail.com>
2021-06-08 15:01:15 +0100

    Changed javascript to an asset

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
         <link href="{{ asset('css/icons.css') }}" rel="stylesheet" >
         @livewireStyles
         <script src="https://kit.fontawesome.com/a357069ed8.js" crossorigin="anonymous"></script>
-        <script src="/js/app.js" defer></script>
+        <script src="{{ asset('js/app.js') }}" defer></script>
         <title>@if (trim($__env->yieldContent('title'))) @yield('title') | @endif Mealing</title>
         @livewireScripts
         <script src="https://cdn.ckeditor.com/ckeditor5/27.1.0/classic/ckeditor.js"></script>
