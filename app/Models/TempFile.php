d077e3f3c648f583a6ee54d16ca3b6d25959e42a
Justin Byrne <theskaterjesus@gmail.com>
2021-06-09 20:26:09 +0100

    Added a temp file model and migration

---
+++ app/Models/TempFile.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TempFile extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'folder',
+        'filename',
+    ];
+}
