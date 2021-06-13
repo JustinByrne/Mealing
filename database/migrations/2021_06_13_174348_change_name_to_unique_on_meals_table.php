e79262af39b7a67cff926d2d51acc9925be9d9ad
Justin Byrne <theskaterjesus@gmail.com>
2021-06-13 18:48:36 +0100

    Changed meal name and sug to be unique

---
+++ database/migrations/2021_06_13_174348_change_name_to_unique_on_meals_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangeNameToUniqueOnMealsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('meals', function (Blueprint $table) {
+            $table->string('name')->unique()->change();
+            $table->string('slug')->unique()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('meals', function (Blueprint $table) {
+            //
+        });
+    }
+}
