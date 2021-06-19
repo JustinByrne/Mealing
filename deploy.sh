e68d4440d97180e261b349012aa44123f7748185
Justin Byrne <theskaterjesus@gmail.com>
2021-06-19 23:26:13 +0100

    Added a deploy script

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+php artisan down
+
+git pull
+
+export COMPOSER_HOME='/tmp/composer'
+composer install --no-interaction --no-dev --prefer-dist
+
+php artisan cache:clear
+php artisan config:clear
+php artisan config:cache
+php artisan -v queue:restart
+php artisan migrate --force
+php artisan up
