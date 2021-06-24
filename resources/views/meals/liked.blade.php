1d1e146058b1cc9b5484e72f9aa2bd566e85a378
Justin Byrne <theskaterjesus@gmail.com>
2021-06-24 22:42:56 +0100

    Changed the height of meals

--- resources/views/meals/liked.blade.php
+++ resources/views/meals/liked.blade.php
@@ -7,7 +7,7 @@
             <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-6 mb-5">
                 @foreach ($meals as $meal)
                     <a href="{{ route('meals.show', $meal) }}">
-                        <div class="w-full rounded-md shadow-md dark:bg-gray-700">
+                        <div class="w-full h-full rounded-md shadow-md bg-white dark:bg-gray-700">
                             <div>
                                 @if ($meal->getMedia()->count() > 0)
                                     <img src="{{ $meal->getFirstMediaUrl('default', 'thumb') }}" class="w-full h-32 object-cover rounded-t-md" alt="{{ $meal->name }}">
@@ -15,7 +15,7 @@
                                     <img src="https://via.placeholder.com/640x360.png?text=No+Image" class="w-full max-h-32 object-cover rounded-t-md" alt="No image available">
                                 @endif
                             </div>
-                            <div class="bg-white p-4 rounded-b-md dark:bg-gray-700 dark:text-gray-200 space-y-2">
+                            <div class="p-4 rounded-b-md dark:text-gray-200 space-y-2">
                                 <p>
                                     {{ $meal->name }}
                                 </p>
