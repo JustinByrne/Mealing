5b5a89e33dc3eaea622484b8086d100ef17641b8
Justin Byrne <theskaterjesus@gmail.com>
2021-06-24 22:20:00 +0100

    fixed missing import

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Symfony\Component\Process\Process;
+use Symfony\Component\Process\Exception\ProcessFailedException;
 
 class WebhookController extends Controller
 {
