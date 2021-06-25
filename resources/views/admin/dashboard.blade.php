cffef0e275555bc6961d767dc7568e99dd59b212
Justin Byrne <theskaterjesus@gmail.com>
2021-06-25 18:18:12 +0100

    Added an empty box to admin dash for the future

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -26,13 +26,13 @@
                 Ingredients
             </p>
         </div>
-        {{-- <div class="shadow-lg rounded-lg p-4 bg-gradient-to-br from-green-400 to-blue-500 h-32">
+        <div class="shadow-lg rounded-lg p-4 bg-gradient-to-br from-green-400 to-blue-500 h-32">
             <p class="font-black text-white text-xl">
-                
+                0
             </p>
             <p class="font-black text-white text-xl">
-                
+                TBC
             </p>
-        </div> --}}
+        </div>
     </div>
 @endsection
