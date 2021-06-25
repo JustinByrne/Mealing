72350e57f4013520be53b4ead46b41b863792157
Justin Byrne <theskaterjesus@gmail.com>
2021-06-25 15:53:48 +0100

    Added counters to admin dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,10 +1,38 @@
 @extends('admin.layout')
 
 @section('admin.content')
-    <div class="grid grid-cols-1 lg:grid-cols-4 p-4">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+    <div class="grid grid-cols-1 lg:grid-cols-4 p-4 gap-4">
+        <div class="shadow-lg rounded-lg p-4 bg-gradient-to-br from-green-400 to-blue-500 h-32">
+            <p class="font-black text-white text-xl">
+                {{ $users }}
+            </p>
+            <p class="font-black text-white text-xl">
+                Users
+            </p>
+        </div>
+        <div class="shadow-lg rounded-lg p-4 bg-gradient-to-br from-green-400 to-blue-500 h-32">
+            <p class="font-black text-white text-xl">
+                {{ $meals }}
+            </p>
+            <p class="font-black text-white text-xl">
+                Meals
+            </p>
+        </div>
+        <div class="shadow-lg rounded-lg p-4 bg-gradient-to-br from-green-400 to-blue-500 h-32">
+            <p class="font-black text-white text-xl">
+                {{ $ingredients }}
+            </p>
+            <p class="font-black text-white text-xl">
+                Ingredients
+            </p>
+        </div>
+        {{-- <div class="shadow-lg rounded-lg p-4 bg-gradient-to-br from-green-400 to-blue-500 h-32">
+            <p class="font-black text-white text-xl">
+                
+            </p>
+            <p class="font-black text-white text-xl">
+                
+            </p>
+        </div> --}}
     </div>
 @endsection
