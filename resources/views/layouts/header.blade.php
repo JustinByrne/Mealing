8c60a6f5d8b649abfff1a8245cd1f817442bf047
Justin Byrne <theskaterjesus@gmail.com>
2021-07-08 18:21:40 +0100

    Fixed incorrect margin in menu

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -41,17 +41,17 @@
                 </a>
                 @if (request()->routeIs('recipes*'))
                     <div class="w-full" x-data="{open: true}">
-                        <div class="w-full p-3 mb-3 font-bold rounded-lg bg-white text-green-700 space-x-2 cursor-pointer" @click="open = !open">
+                        <div class="w-full p-3 font-bold rounded-lg bg-white text-green-700 space-x-2 cursor-pointer" @click="open = !open">
                 @else
                     <div class="w-full" x-data="{open: false}">
-                        <div class="w-full p-3 mb-3 font-bold rounded-lg hover:bg-white hover:text-green-700 space-x-2 cursor-pointer" @click="open = !open">
+                        <div class="w-full p-3 font-bold rounded-lg hover:bg-white hover:text-green-700 space-x-2 cursor-pointer" @click="open = !open">
                 @endif
                         <i class="fas fa-hamburger"></i>
                         <span>
                             Recipes
                         </span>
                     </div>
-                    <div :class="{'hidden': !open}" class="flex flex-col space-y-2">
+                    <div :class="{'hidden': !open}" class="flex flex-col space-y-2 mt-3">
                         <a
                             href="{{ route('recipes.index') }}"
                             @if (request()->routeIs('recipes.index'))
