8fafc4e9942e918840c07aba6a9e9cbbb1998b0f
Justin Byrne <theskaterjesus@gmail.com>
2021-07-14 19:50:15 +0100

    Moved search box into a div

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,15 +23,17 @@
             <main class="p-4 lg:p-6 w-full lg:ml-72">
                 <!-- Search Bar -->
                 <div class="flex flex-col lg:flex-row justify-between lg:mb-4">
-                    <div></div>
-                    {{-- <form action="" method="POST">
-                        <div class="relative">
-                            <button type="submit" class="absolute p-3 bg-green-700 bg-opacity-20 rounded-lg text-center text-green-700 dark:bg-opacity-30">
-                                <i class="fas fa-search"></i>
-                            </button>
-                            <input class="w-full max-w-xs pl-12 py-3 border-0 bg-green-700 bg-opacity-10 rounded-lg placeholder-gray-400 dark:bg-opacity-30 dark:text-gray-200" type="text" placeholder="Search..." name="s">
-                        </div>
-                    </form> --}}
+                    <div>
+                        {{-- <form action="" method="POST">
+                            @csrf
+                            <div class="relative">
+                                <button type="submit" class="absolute p-3 bg-green-700 bg-opacity-20 rounded-lg text-center text-green-700 dark:bg-opacity-30">
+                                    <i class="fas fa-search"></i>
+                                </button>
+                                <input class="w-full max-w-xs pl-12 py-3 border-0 bg-green-700 bg-opacity-10 rounded-lg placeholder-gray-400 dark:bg-opacity-30 dark:text-gray-200" type="text" placeholder="Search..." name="s">
+                            </div>
+                        </form> --}}
+                    </div>
                     <div class="hidden lg:block text-right relative" x-data="{ open: false }" @click.away="open = false">
                         <div class="text-gray-600 flex items-center space-x-2 cursor-pointer dark:text-gray-200" @click="open = !open">
                             <img class="rounded-full w-10 h-10 border-2 border-transparent hover:border-orange-300" src="{{ Auth::user()->getAvatar() }}" alt="{{ Auth::user()->name }}">
