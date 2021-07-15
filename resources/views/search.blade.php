5c60fef50bd3a21223dc684125c36f36de5e5a5f
Justin Byrne <theskaterjesus@gmail.com>
2021-07-15 21:04:46 +0100

    Added results blade

---
+++ resources/views/search.blade.php
@@ -0,0 +1,142 @@
+@extends('layouts.app')
+
+@section('title', 'Search results for ' . $s)
+
+@section('content')
+<div class="flex flex-col lg:flex-row lg:space-x-6">
+    <div class="w-full lg:w-4/5 order-2 lg:order-1">
+        <p class="font-bold mb-5 dark:text-gray-200">
+            Search results for <em>{{ $s }}</em>
+        </p>
+        
+        @if ($recipes->count() == 0)
+            <p class="mb-5 dark:text-gray-200">
+                No results found
+            </p>
+        @endif
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6 mb-5">
+            @foreach ($recipes as $recipe)
+                <a href="{{ route('recipes.show', $recipe) }}">
+                    <div class="w-full h-full rounded-md shadow-md bg-white dark:bg-gray-700">
+                        <div>
+                            @if ($recipe->getMedia()->count() > 0)
+                                <img src="{{ $recipe->getFirstMediaUrl('default', 'thumb') }}" class="w-full h-32 object-cover rounded-t-md" alt="{{ $recipe->name }}">
+                            @else
+                                <img src="https://via.placeholder.com/640x360.png?text=No+Image" class="w-full max-h-32 object-cover rounded-t-md" alt="No image available">
+                            @endif
+                        </div>
+                        <div class="p-4 rounded-b-md dark:text-gray-200 space-y-2">
+                            <p>
+                                {{ $recipe->name }}
+                            </p>
+                            <p class="text-sm text-yellow-400" title="{{ $recipe->avg_rating > 0 ? $recipe->avg_rating : '0' }}">
+                                @for ($x = 0; $x < 5; $x++)
+                                    @if (floor($recipe->avg_rating)-$x >= 1)
+                                        <i class="fas fa-star"></i>
+                                    @elseif ($recipe->avg_rating-$x > 0)
+                                        <i class="fas fa-star-half-alt"></i>
+                                    @else
+                                        <i class="far fa-star"></i>
+                                    @endif
+                                @endfor
+                            </p>
+                            <p>
+                                Serves: {{ $recipe->servings }}
+                            </p>
+                        </div>
+                    </div>
+                </a>
+            @endforeach
+        </div>
+        {{ $recipes->links() }}
+    </div>
+    <div class="w-full lg:w-1/5 order-1 mb-3 lg:mb-0" x-data="{open: false}">
+        <div class="w-full rounded-md shadow-md bg-white dark:bg-gray-700">
+            <div class="flex justify-between p-4 border-b border-gray-200">
+                <p class="font-bold dark:text-gray-200 self-center">
+                    Filter
+                </p>
+                <div class="lg:hidden">
+                    <button class="border border-gray-100 px-2 pt-1 rounded-lg dark:text-gray-200" @click="open = !open">
+                        <i class="fas fa-bars"></i>
+                    </button>
+                </div>
+            </div>
+            {{-- <div class="hidden lg:block" :class="{'hidden': !open}">
+                <div class="p-4">
+                    <div class="flex flex-row justify-between">
+                        <div class="font-bold mb-3 dark:text-gray-200">
+                            Rating
+                        </div>
+                    </div>
+                    <div class="flex flex-col">
+                        <p class="dark:text-gray-200">
+                            <input type="checkbox" checked class="rounded mr-2 text-green-700 focus:outline-none focus:ring-1 focus:border-green-500 focus:ring-green-500 dark:bg-gray-900 dark:border-transparent dark:text-gray-200 dark:focus:bg-green-700">
+                            All
+                        </p>
+                        <p class="dark:text-gray-200">
+                            <input type="checkbox" class="rounded mr-2 text-green-700 focus:outline-none focus:ring-1 focus:border-green-500 focus:ring-green-500 dark:bg-gray-900 dark:border-transparent dark:text-gray-200 dark:focus:bg-green-700">
+                            <span class="text-sm text-yellow-400">
+                                <i class="fas fa-star"></i>
+                                <i class="far fa-star"></i>
+                                <i class="far fa-star"></i>
+                                <i class="far fa-star"></i>
+                                <i class="far fa-star"></i>
+                            </span>
+                        </p>
+                        <p class="dark:text-gray-200">
+                            <input type="checkbox" class="rounded mr-2 text-green-700 focus:outline-none focus:ring-1 focus:border-green-500 focus:ring-green-500 dark:bg-gray-900 dark:border-transparent dark:text-gray-200 dark:focus:bg-green-700">
+                            <span class="text-sm text-yellow-400">
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="far fa-star"></i>
+                                <i class="far fa-star"></i>
+                                <i class="far fa-star"></i>
+                            </span>
+                        </p>
+                        <p class="dark:text-gray-200">
+                            <input type="checkbox" class="rounded mr-2 text-green-700 focus:outline-none focus:ring-1 focus:border-green-500 focus:ring-green-500 dark:bg-gray-900 dark:border-transparent dark:text-gray-200 dark:focus:bg-green-700">
+                            <span class="text-sm text-yellow-400">
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="far fa-star"></i>
+                                <i class="far fa-star"></i>
+                            </span>
+                        </p>
+                        <p class="dark:text-gray-200">
+                            <input type="checkbox" class="rounded mr-2 text-green-700 focus:outline-none focus:ring-1 focus:border-green-500 focus:ring-green-500 dark:bg-gray-900 dark:border-transparent dark:text-gray-200 dark:focus:bg-green-700">
+                            <span class="text-sm text-yellow-400">
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="far fa-star"></i>
+                            </span>
+                        </p>
+                        <p class="dark:text-gray-200">
+                            <input type="checkbox" class="rounded mr-2 text-green-700 focus:outline-none focus:ring-1 focus:border-green-500 focus:ring-green-500 dark:bg-gray-900 dark:border-transparent dark:text-gray-200 dark:focus:bg-green-700">
+                            <span class="text-sm text-yellow-400">
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                            </span>
+                        </p>
+                    </div>
+                </div>
+                <hr class="mx-3">
+                <div class="grid grid-cols-2 gap-4 p-4">
+                    <button class="w-full lg:w-auto rounded shadow-md py-2 px-4 bg-green-700 text-white hover:bg-green-500">
+                        Filter
+                    </button>
+                    <button class="w-full lg:w-auto rounded shadow-md py-2 px-4 bg-gray-400 text-white hover:bg-gray-300">
+                        Clear
+                    </button>
+                </div>
+            </div> --}}
+        </div>
+    </div>
+</div>
+@endsection
