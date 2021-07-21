92949092d3840be425f0042a21a911108ba2029e
Justin Byrne <theskaterjesus@gmail.com>
2021-07-21 19:50:58 +0100

    Show that a user needs to verify email first

--- TODO.md
+++ TODO.md
@@ -3,5 +3,4 @@
 - Create a good README.md file
 - Add ability to restrict menus by allergens
 - Add filters to recipe index and search pages
-- Add to user table if email is verified
 - 
--- resources/views/livewire/admin/users/approval.blade.php
+++ resources/views/livewire/admin/users/approval.blade.php
@@ -1,7 +1,11 @@
 <div>
-    @if ($user->approved != 1)
+    @if ($user->approved != 1 && ! is_null($user->email_verified_at))
         <button wire:click="approve" class="w-full lg:w-auto rounded shadow-md py-1 px-2 bg-green-700 text-white hover:bg-green-500 text-xs">
             Approve
         </button>
+    @elseif (is_null($user->email_verified_at))
+        <span class="w-full lg:w-auto rounded shadow-md py-1 px-2 bg-green-700 text-white hover:bg-green-500 text-xs">
+            Not Verified
+        </span>
     @endif
 </div>
